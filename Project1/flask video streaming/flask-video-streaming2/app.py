7b4684065a90b73db8106adcbb6f960c2d92be2b
sehooh5 <sehooh5@gmail.com>
2020-09-24 16:59:15 +0900

    update flask streaming

--- Project1/flask video streaming/flask-video-streaming2/app.py
+++ Project1/flask video streaming/flask-video-streaming2/app.py
@@ -3,11 +3,15 @@ from importlib import import_module
 import os
 from flask import Flask, render_template, Response, request
 
-# import camera driver
-if os.environ.get('CAMERA'):
-    Camera = import_module('camera_' + os.environ['CAMERA']).Camera
-else:
-    from camera import Camera
+# 1. import camera driver
+# if os.environ.get('CAMERA'):
+#     Camera = import_module('camera_' + os.environ['CAMERA']).Camera
+# else:
+#     from camera import Camera
+
+# 2. 카메라 opencv 로 처음부터 지정
+Camera = import_module('camera_opencv').Camera
+
 
 # Raspberry Pi camera module (requires picamera package)
 # from camera_pi import Camera
@@ -26,10 +30,9 @@ def camera():
     """Camera1 streaming"""
     # url 의 파라미터 값을 가져오는 방법
     no = request.args.get('no')
-    print('number = '+request.args.get('no'))
+    # print('number = '+request.args.get('no'))
     if no == '1':
         os.environ['OPENCV_CAMERA_SOURCE'] = 'rtsp://keti:keti1234@192.168.100.70:8810/videoMain'
-        print(os.environ['OPENCV_CAMERA_SOURCE'])
     elif no == '2':
         os.environ['OPENCV_CAMERA_SOURCE'] = 'rtsp://keti:keti1234@192.168.100.60:8805/videoMain'
 
