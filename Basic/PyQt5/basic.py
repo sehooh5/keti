1e0cbc72b7d9e3dfedaf4bf5fc070b5c9358f977
sehooh5 <sehooh5@gmail.com>
2021-06-09 16:05:10 +0900

    update grafana tab

--- Basic/PyQt5/basic.py
+++ Basic/PyQt5/basic.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import sys
 from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, \
-    QTabWidget, QVBoxLayout, QPushButton, QCheckBox, QRadioButton
+    QTabWidget, QVBoxLayout, QHBoxLayout, QPushButton, QCheckBox, QRadioButton
 
 
 class MainWindow(QMainWindow):
@@ -9,12 +9,12 @@ class MainWindow(QMainWindow):
         super().__init__()
         # 윈도우 설정
         self.setGeometry(50, 50, 100, 100)  # x, y, w, h
-        self.setWindowTitle('Status Window')
+        self.setWindowTitle('Basic Application')
 
         # Tab Widget
         tabs = QTabWidget()
-        tabs.addTab(self.make_tab1(), 'One')
-        tabs.addTab(self.make_tab2(), 'Two')
+        tabs.addTab(self.make_tab1(), 'Function')
+        tabs.addTab(self.make_tab2(), 'Monitoring')
 
         # QMainWindow 추가
         self.setCentralWidget(tabs)
@@ -23,19 +23,33 @@ class MainWindow(QMainWindow):
     # 첫번째 탭 생성함수
     def make_tab1(self):
         # 버튼 객체 만들기
-        button1 = QPushButton('버튼1', self)
-        button2 = QPushButton('버튼2', self)
-        button3 = QPushButton('버튼3', self)
+        button1 = QPushButton('Login', self)
+        button2 = QPushButton('File', self)
+        button3 = QPushButton('Docker Image Build', self)
+        button4 = QPushButton('Docker Image push', self)
+        button5 = QPushButton('Kubernetes Apply', self)
 
         # 레이아웃 만들기
         vbox = QVBoxLayout()
+        vbox.addStretch(1)
         vbox.addWidget(button1)
+        vbox.addStretch(1)
         vbox.addWidget(button2)
+        vbox.addStretch(1)
         vbox.addWidget(button3)
+        vbox.addStretch(1)
+        vbox.addWidget(button4)
+        vbox.addStretch(1)
+        vbox.addWidget(button5)
+        vbox.addStretch(6)
+
+        hbox = QHBoxLayout()
+        hbox.addLayout(vbox)
+        hbox.addStretch(8)
 
         # 위젯에 레이아웃 추가하기
         tab = QWidget()
-        tab.setLayout(vbox)
+        tab.setLayout(hbox)
         return tab
 
     def make_tab2(self):
