914f30849820cf0a60982038603c60cd9ef88a6b
sehooh5 <sehooh5@gmail.com>
2021-08-09 18:03:52 +0900

    update k8s_join using python

--- Project1/api_k8s/k8s_join.py
+++ Project1/api_k8s/k8s_join.py
@@ -3,21 +3,29 @@ import sys
 import paramiko
 import getpass
 import time
-# token 210809 : k1caaa.ru6kcyp598l0hx6z
-
+# kubeadm join 192.168.0.29:6443 --token m33kvg.xta9556i5lobye1v \
+#    --discovery-token-ca-cert-hash sha256:288c16783a76640c705faa77c170272e02b06668d3b8b00732d3bad3c0527cb1
 
 # 다른 서버에 명령 보낼때 사용
 cli = paramiko.SSHClient()
-cli.set_missing_host_key_policy(paramiko.AutoAddPolicy)
+cli.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
 # 임시로 파이썬 실행시 아규먼트로 전달
 wip = sys.argv[1]
 wname = sys.argv[2]
 wpwd = sys.argv[3]
-w_input = "sudo kubeadm join 192.168.100.5:6443 --token k1caaa.ru6kcyp598l0hx6z --discovery-token-ca-cert-hash sha256:7f1758ca4cfd117cda27099644cbe4ef672559a47ab33dce8dd87ddf2e8bea1c"
+w_input = "sudo kubeadm join 192.168.0.29:6443 --token m33kvg.xta9556i5lobye1v \
+    --discovery-token-ca-cert-hash sha256:288c16783a76640c705faa77c170272e02b06668d3b8b00732d3bad3c0527cb1"
 cli.connect(wip, port=22, username=wname, password=wpwd)
-stdin, stdout, stderr = cli.exec_command(w_input)
-time.sleep(1.0)
+
+stdin, stdout, stderr = cli.exec_command(w_input, get_pty=True)
+stdin.write('keti\n')
+stdin.flush()
+
+lines = stdout.readlines()
+print(''.join(lines))
+
+time.sleep(2.0)
 cli.close()
 
 # 1. 인터넷사용
