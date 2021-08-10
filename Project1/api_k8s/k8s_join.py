d010886c84ebee9065ad4679e6e70d299badf6e0
sehooh5 <sehooh5@gmail.com>
2021-08-10 17:20:07 +0900

    k8s join

--- Project1/api_k8s/k8s_join.py
+++ Project1/api_k8s/k8s_join.py
@@ -11,9 +11,11 @@ cli = paramiko.SSHClient()
 cli.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
 # 임시로 파이썬 실행시 아규먼트로 전달
+# 나중에 입력받는 값으로 바꿔줘야함
 wip = sys.argv[1]
 wname = sys.argv[2]
 wpwd = sys.argv[3]
+# 나중에 마스터 설정할때 나오는 명령문으로 바꿔줘야함
 w_input = "sudo kubeadm join 192.168.0.29:6443 --token m33kvg.xta9556i5lobye1v \
     --discovery-token-ca-cert-hash sha256:288c16783a76640c705faa77c170272e02b06668d3b8b00732d3bad3c0527cb1"
 cli.connect(wip, port=22, username=wname, password=wpwd)
@@ -28,8 +30,7 @@ print(''.join(lines))
 time.sleep(2.0)
 cli.close()
 
-# 1. 인터넷사용
-# os.system(f"kubeadm join 10.244.0.1 --token k1caaa.ru6kcyp598l0hx6z")
-
-# 2. 이더넷 사용
-# os.system(f"kubeadm join 192.168.100.5:6443 --token k1caaa.ru6kcyp598l0hx6z")
+# 나중에 위 명령문 실행 후 아래 명령문도 실행해줘야된다
+# $ mkdir -p $HOME/.kube
+# $ sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+# $ sudo chown $(id -u):$(id -g) $HOME/.kube/config
