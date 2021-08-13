4ce0ecb251721b1a2896cab451a9dccb4e564822
sehooh5 <sehooh5@gmail.com>
2021-08-13 17:34:34 +0900

    udpate repo

---
+++ Project1/api_k8s/docker/build.py
@@ -0,0 +1,14 @@
+import os
+import sys
+
+
+# url = sys.argv[1]
+# file = sys.argv[2]
+# docker_id = sys.argv[3]
+# sw_name = sys.argv[4]
+
+
+def build(url, file, docker_id, sw_name):
+
+    os.system(
+        f"docker build -f {url}/{file} -t {docker_id}/{sw_name}:latest .")
