7c96f19b0a8d2dea0479ffa8698e7f2bf4c4c8e0
sehooh5 <58541635+sehooh5@users.noreply.github.com>
2022-08-18 12:48:50 +0900

    update gps_data.py

--- Project1/3년차/gps/gps_data.py
+++ Project1/3년차/gps/gps_data.py
@@ -7,6 +7,9 @@ def parseGPS(message):
         print(msg)
         print (f"Timestamp: {msg.timestamp} -- Lat: {msg.lat} {msg.lat_dir} " \
               f"-- Lon: {msg.lon} {msg.lon_dir} -- Altitude:{msg.altitude} {msg.altitude_units}")
+    else:
+        print("pass")
+
 serialPort = serial.Serial("/dev/ttyUSB0", 9600, timeout=0.5)
 while True:
     msg = serialPort.readline()
