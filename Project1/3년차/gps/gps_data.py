370ef00f8128cb24f542b755335229caa38dc51e
sehooh5 <58541635+sehooh5@users.noreply.github.com>
2022-08-18 12:53:19 +0900

    update gps_data.py

--- Project1/3년차/gps/gps_data.py
+++ Project1/3년차/gps/gps_data.py
@@ -2,13 +2,11 @@ import serial
 import pynmea2
 
 def parseGPS(message):
-    if str.find('GGA') > 0:
-        msg = pynmea2.parse(str)
+    if message.find('GGA') > 0:
+        msg = pynmea2.parse(message)
         print(msg)
         print (f"Timestamp: {msg.timestamp} -- Lat: {msg.lat} {msg.lat_dir} " \
               f"-- Lon: {msg.lon} {msg.lon_dir} -- Altitude:{msg.altitude} {msg.altitude_units}")
-    else:
-        print("pass")
 
 serialPort = serial.Serial("/dev/ttyUSB0", 9600, timeout=0.5)
 while True:
--- Project1/3년차/gps/pynmea_test.py
+++ Project1/3년차/gps/pynmea_test.py
@@ -1,9 +1,9 @@
 import pynmea2
 
 str = "$GPGGA,071341.000,3734.8517,N,12653.2973,E,1,8,0.99,79.5,M,19.2,M,,*61"
-print(str)
+print(str.find('GGA'))
 
 msg = pynmea2.parse(str)
-print(msg)
+
 print (f"Timestamp: {msg.timestamp} -- Lat: {msg.lat} {msg.lat_dir} " \
               f"-- Lon: {msg.lon} {msg.lon_dir} -- Altitude:{msg.altitude} {msg.altitude_units}")
