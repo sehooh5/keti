d32f7c3d04f4e5ab8a231dbdfb480e362a0edd28
sehooh5 <58541635+sehooh5@users.noreply.github.com>
2022-08-23 17:12:06 +0900

    update gps python

--- Project1/3년차/gps/gps_data.py
+++ Project1/3년차/gps/gps_data.py
@@ -1,16 +1,26 @@
 import serial
 import pynmea2
+import datetime
+
 
 def parseGPS(message):
     message = message.decode('utf-8')
     if (message[0:6] == "$GPGGA"):
-        print("IN")
         msg = pynmea2.parse(message)
-        print(msg)
-        print (f"Timestamp: {msg.timestamp} -- Lat: {msg.lat} {msg.lat_dir} " \
-              f"-- Lon: {msg.lon} {msg.lon_dir} -- Altitude:{msg.altitude} {msg.altitude_units}")
+
+        gps_time = datetime.datetime.utcnow()
+        gps_lat = msg.lat
+        gps_lon = msg.lon
+        gps_lat_dir = msg.lat_dir
+        gps_lon_dir = msg.lon_dir
+        gps_alt = msg.altitude
+        gps_alt_units = msg.altitude_units
+
+        print (f"Timestamp: {gps_time} -- Lat: {gps_lat} {gps_lat_dir} " \
+              f"-- Lon: {gps_lon} {gps_lon_dir} -- Altitude:{gps_alt} {gps_alt_units}")
     else :
         print("PASS")
+
 serialPort = serial.Serial("/dev/ttyUSB0", 9600, timeout=0.5)
 while True:
     msg = serialPort.readline()
