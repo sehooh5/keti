6d3edb69afc38bb6d0229c32a7b08776f1612d1a
sehooh5 <58541635+sehooh5@users.noreply.github.com>
2022-08-24 16:22:20 +0900

    update gps python

--- Project1/3년차/gps/gps_client.py
+++ Project1/3년차/gps/gps_client.py
@@ -2,8 +2,27 @@ import serial
 import pynmea2
 import datetime
 import os
+import socket
+import json
+import threading
+import subprocess
+
+port = 8080
 
 def parseGPS(message):
+
+
+    data = {
+        'type': 'device',
+        'd_id': 'd1',
+    }
+    json_data = json.dumps(data)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect(('192.168.0.20', port))
+    sock.send(json_data.encode('utf-8'))
+    recvData = sock.recv(1024)
+    data = recvData.decode(('utf-8'))
+    print(data)
 
     message = message.decode('utf-8')
     if (message[0:6] == "$GPGGA"):
--- Project1/3년차/gps/gps_server.py
+++ Project1/3년차/gps/gps_server.py
@@ -1,5 +1,24 @@
+import socket
+import json
 
+port = 8080
 
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind(('192.168.0.20', port))
 
+while True:
+    server_socket.listen()
+    client_socket, addr = server_socket.accept()
 
+    # 클라이언트로부터 메시지 받기
+    recvData = client_socket.recv(65535)
+    json_data = json.loads(recvData.decode(('utf-8')))
+    type = json_data['type']
+    print(json_data)
 
+    if type == 'gps':
+        print('gps 에서 데이터가 전달되었습니다!')
+        sendData = 'GPS data uploaded!'
+        client_socket.send(sendData.encode('utf-8'))
+    elif type == 'vms':
+        print('vms 에서 요청이 왔습니다!')
