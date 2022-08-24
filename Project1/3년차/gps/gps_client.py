8bd3b5810a5292ade5267b8236e9675c9605dda0
sehooh5 <58541635+sehooh5@users.noreply.github.com>
2022-08-24 17:20:16 +0900

    update gps client for ip

--- Project1/3년차/gps/gps_client.py
+++ Project1/3년차/gps/gps_client.py
@@ -9,17 +9,6 @@ import subprocess
 
 port = 8088
 
-data = {
-    'type': 'device',
-    'd_id': 'd1',
-}
-json_data = json.dumps(data)
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect(('123.214.186.162', port))
-sock.send(json_data.encode('utf-8'))
-recvData = sock.recv(1024)
-data = recvData.decode(('utf-8'))
-
 def parseGPS(message):
 
 
