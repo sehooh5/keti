08f2f44adb3434ba5b777bbad522b9756df2ae02
sehooh5 <58541635+sehooh5@users.noreply.github.com>
2022-08-26 12:44:28 +0900

    update gps client server using rest

---
+++ Project1/3년차/gps/rest/gps_client.py
@@ -0,0 +1,64 @@
+import serial
+import pynmea2
+import datetime
+import os
+from flask import Flask, render_template, Response, request, jsonify
+import json
+import requests
+import response
+
+app = Flask(__name__)
+port = 5885
+
+@app.route('/')
+def index():
+    return "index"
+
+@app.route('/get_gpsInfo', method=['GET'])
+def gps():
+    serialPort = serial.Serial("/dev/ttyUSB0", 9600, timeout=5)
+    msg = serialPort.readline()
+    res = parseGPS(msg)
+    print(res)
+
+    return res
+
+app.run(host="192.168.225.27",port=port)
+
+def parseGPS(message):
+
+    message = message.decode('utf-8')
+    if (message[0:6] == "$GPGGA"):
+        msg = pynmea2.parse(message)
+
+        gps_id = os.getlogin() # 서버의 username
+        gps_time = datetime.datetime.utcnow().strftime("%m/%d/%Y, %H:%M:%S") # UTC 시간
+        gps_lat = msg.lat
+        gps_lon = msg.lon
+        gps_lat_dir = msg.lat_dir
+        gps_lon_dir = msg.lon_dir
+        gps_alt = msg.altitude
+        gps_alt_units = msg.altitude_units
+
+        data = {
+            'type': 'gps',
+            'gps_id': gps_id,
+            'gps_time': gps_time,
+            'gps_lat': gps_lat,
+            'gps_lon': gps_lon,
+            'gps_lat_dir': gps_lat_dir,
+            'gps_lon_dir': gps_lon_dir,
+            'gps_alt': gps_alt,
+            'gps_alt_units': gps_alt_units
+        }
+        json_data = json.dumps(data)
+        print(json_data)
+    return json_data
+        #print (f"ID: {gps_id} -- Timestamp: {gps_time} -- Lat: {gps_lat} {gps_lat_dir} " \
+        #      f"-- Lon: {gps_lon} {gps_lon_dir} -- Altitude:{gps_alt} {gps_alt_units}")
+
+
+#serialPort = serial.Serial("/dev/ttyUSB0", 9600, timeout=5) # 임의로 5초 나중에 바꿔야함 1초로
+#while True:
+#    msg = serialPort.readline()
+#    parseGPS(msg)
---
+++ Project1/3년차/gps/rest/gps_server.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, Response, request, jsonify
+import json
+import requests
+import response
+
+app = Flask(__name__)
+port = 8088
+
+
+@app.route('/')
+def index():
+    return "index"
+
+@app.route('/gps', method=['POST'])
+def gps():
+    json_data = request.get_json(silent=True)
+    # did 추출
+    did = json_data['did']
+    # 지금은 did 를 사용 안하지만 나중에는 이 아이디로 device ip 를 찾아서 요청해야함
+    dip = "192.168.225.27:5885"
+    res = requests.get(f"{dip}/get_gpsInfo")
+
+    return res
+
+app.run(host="123.214.186.162",port=port)
+
