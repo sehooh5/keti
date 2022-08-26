4b05d34101e288e9775919772c0c9703b228f0d3
sehooh5 <58541635+sehooh5@users.noreply.github.com>
2022-08-26 15:54:56 +0900

    update gps client server using rest

--- Project1/3년차/gps/rest/gps_client.py
+++ Project1/3년차/gps/rest/gps_client.py
@@ -1,39 +1,24 @@
+import requests
 import serial
 import pynmea2
 import datetime
 import os
-from flask import Flask, render_template, Response, request, jsonify
+import socket
 import json
-import requests_test
-import response
 
-app = Flask(__name__)
-port = 5885
+url = "http://123.214.186.162:8088/gps"
 
-@app.route('/')
-def index():
-    return "index"
-
-@app.route('/get_gpsInfo', methods=['GET'])
-def gps():
-    serialPort = serial.Serial("/dev/ttyUSB0", 9600, timeout=5)
-    msg = serialPort.readline()
-    res = parseGPS(msg)
-    print(res)
-
-    return res
-
-app.run(host="192.168.225.27",port=port)
-#192.168.225.27
+data = {
+    "did": "keti0"
+}
 
 def parseGPS(message):
-
     message = message.decode('utf-8')
     if (message[0:6] == "$GPGGA"):
         msg = pynmea2.parse(message)
 
-        gps_id = os.getlogin() # 서버의 username
-        gps_time = datetime.datetime.utcnow().strftime("%m/%d/%Y, %H:%M:%S") # UTC 시간
+        gps_id = os.getlogin()  # 서버의 username
+        gps_time = datetime.datetime.utcnow().strftime("%m/%d/%Y, %H:%M:%S")  # UTC 시간
         gps_lat = msg.lat
         gps_lon = msg.lon
         gps_lat_dir = msg.lat_dir
@@ -53,13 +38,10 @@ def parseGPS(message):
             'gps_alt_units': gps_alt_units
         }
         json_data = json.dumps(data)
-        print(json_data)
-    return json_data
-        #print (f"ID: {gps_id} -- Timestamp: {gps_time} -- Lat: {gps_lat} {gps_lat_dir} " \
-        #      f"-- Lon: {gps_lon} {gps_lon_dir} -- Altitude:{gps_alt} {gps_alt_units}")
+        res = requests.post(url, json=data)
+        # print("response : ", res)
 
-
-#serialPort = serial.Serial("/dev/ttyUSB0", 9600, timeout=5) # 임의로 5초 나중에 바꿔야함 1초로
-#while True:
-#    msg = serialPort.readline()
-#    parseGPS(msg)
+serialPort = serial.Serial("/dev/ttyUSB0", 9600, timeout=5) # 임의로 5초 나중에 바꿔야함 1초로
+while True:
+    msg = serialPort.readline()
+    parseGPS(msg)
--- Project1/3년차/gps/rest/gps_server.py
+++ Project1/3년차/gps/rest/gps_server.py
@@ -1,12 +1,17 @@
 from flask import Flask, render_template, Response, request, g, jsonify
 from flask_cors import CORS, cross_origin
 import json
-import response
+import sqlite3
 
 app = Flask(__name__)
 CORS(app)
 port = 8088
 
+# DB 생성 (오토 커밋)
+conn = sqlite3.connect("tcp_test.db", isolation_level=None)
+
+# 커서 획득
+c = conn.cursor()
 
 @app.route('/')
 def index():
@@ -16,18 +21,48 @@ def index():
 def gps():
     global json_data
     json_data = request.get_json(silent=True)
+    gps_id = json_data['gps_id']
+    gps_lat = json_data['gps_lat']
+    gps_lat_dir = json_data['gps_lat_dir']
+    gps_lon = json_data['gps_lon']
+    gps_lon_dir = json_data['gps_lon_dir']
+    gps_alt = json_data['gps_alt']
+    gps_alt_units = json_data['gps_alt_units']
+    gps_time = json_data['gps_time']
+
+    # device table 생성 후 데이터 저장
+    c.execute(f'select name from sqlite_master where type="table" and name="{gps_id}"')
+    dt_exist = c.fetchone()
+    # d_id 테이블이 없으면 테이블 생성
+    if dt_exist == None:
+        print(f"{gps_id} 테이블 생성")
+        c.execute(f"CREATE TABLE IF NOT EXISTS {gps_id} \
+                        (id integer PRIMARY KEY, gps_lat text, gps_lat_dir text, gps_lon text, gps_lon_dir text, gps_alt text, gps_alt_units text, gps_time text)")
+    c.execute((f"SELECT COUNT(*) from {gps_id}"))
+    count = c.fetchall()[0][0]
+    if count == 0:
+        mid = 1
+        # print(type(mid))
+    else:
+        c.execute(f"SELECT max(id) FROM {gps_id}")
+        mid = c.fetchone()[0] + 1  # 가장 큰 id 값
+
+        c.execute(f"SELECT min(id) FROM {gps_id}")
+        delete_id = c.fetchone()[0]
+        c.execute(f"DELETE FROM {gps_id} WHERE id=?", (delete_id,))
+
+
+    c.execute(f"INSERT INTO {gps_id} \
+                VALUES(?,?,?,?,?,?,?,?)", (mid, gps_lat, gps_lat_dir, gps_lon, gps_lon_dir, gps_alt, gps_alt_units, gps_time))
 
     return "OK"
 
 @app.route('/get_gpsData', methods=['GET'])
 def get_gpsData():
     did = request.args.get('did')
-    print(did)
+    json_data['gps_id']
 
-    if json_data != 'Null' :
-        return json_data
-    else:
-        return "Null of json_data"
+    return json_data
 
 app.run(host="123.214.186.162",port=port)
 
