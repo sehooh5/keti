c2f0e9ef454b5a490d347e6d38fd95729f1ca996
sehooh5 <58541635+sehooh5@users.noreply.github.com>
2022-08-26 15:01:34 +0900

    update gps client server using rest

--- Project1/3년차/gps/rest/gps_server.py
+++ Project1/3년차/gps/rest/gps_server.py
@@ -15,7 +15,7 @@ def index():
 @app.route('/gps', methods=['POST'])
 def gps():
     json_data = request.get_json(silent=True)
-    print(json_data)
+    # print(json_data)
     # did 추출
     # did = json_data['did']
     # 지금은 did 를 사용 안하지만 나중에는 이 아이디로 device ip 를 찾아서 요청해야함
@@ -23,7 +23,7 @@ def gps():
 
     return "gps data uploaded!"
 
-@app.route('/gps', methods=['GET'])
+@app.route('/get_gpsData', methods=['GET'])
 def get_gpsData():
     print(g.gps_data)
 
