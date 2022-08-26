cea49f43f7369944c0f9bbae13d4db2cab0736bc
sehooh5 <58541635+sehooh5@users.noreply.github.com>
2022-08-26 14:32:12 +0900

    update gps client server using rest

--- Project1/3년차/gps/rest/gps_server.py
+++ Project1/3년차/gps/rest/gps_server.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, Response, request, jsonify
 from flask_cors import CORS, cross_origin
 import json
-import requests_test
 import response
 
 app = Flask(__name__)
