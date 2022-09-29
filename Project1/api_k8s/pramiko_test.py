39260b7dbaf630d24adcbe31596977134bc3df96
sehooh5 <58541635+sehooh5@users.noreply.github.com>
2022-09-29 12:35:40 +0900

    update remove_edge about paramiko auth in pramiko_test.py

--- Project1/api_k8s/pramiko_test.py
+++ Project1/api_k8s/pramiko_test.py
@@ -4,7 +4,7 @@ import paramiko
 cli = paramiko.SSHClient()
 cli.set_missing_host_key_policy(paramiko.AutoAddPolicy)
 
-cli.connect('192.168.0.25', port=22, username="keti1", password="keti")
+cli.connect('192.168.0.28', port=22, username="keti2", password="keti")
 stdin, stdout, stderr = cli.exec_command("hostname", get_pty=True)
 stdin.write('keti\n')
 stdin.flush()
