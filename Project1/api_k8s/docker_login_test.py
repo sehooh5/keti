3a2c3f9a739b97a524ac0620ec9e2df4441289b4
sehooh5 <58541635+sehooh5@users.noreply.github.com>
2022-09-30 15:44:20 +0900

    update docker login test

--- Project1/api_k8s/docker_login_test.py
+++ Project1/api_k8s/docker_login_test.py
@@ -2,7 +2,7 @@ import os
 import subprocess
 
 # 로그인 되어있는지 확인
-d_login_status=subprocess.check_output("docker info | grep Username", shell=True, errors="non-zero").decode('utf-8')
+d_login_status=subprocess.check_output("docker info | grep Username", shell=True, errors=None).decode('utf-8')
 #print(d_login_status)
 
 # 로그인 시도
