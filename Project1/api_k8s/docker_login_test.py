6b71de91f624db915cb3bce8ea7acdd2261c39fe
sehooh5 <58541635+sehooh5@users.noreply.github.com>
2022-09-30 16:22:24 +0900

    update docker login test

--- Project1/api_k8s/docker_login_test.py
+++ Project1/api_k8s/docker_login_test.py
@@ -3,12 +3,14 @@ import subprocess
 
 # 로그인 되어있는지 확인
 try:
-    subprocess.check_call(["docker","info","|", "grep", "Username"])
+    print("try!!")
+    d_login_status = subprocess.check_output("docker info | grep Username", shell=True).decode('utf-8')
+    print(d_login_status)
 except subprocess.CalledProcessError:
-    print(" Non-zero status  ")
+    print("except!!")
+    print("Non-zero status !! ")
 
-# d_login_status=subprocess.check_output("docker info | grep Username", shell=True).decode('utf-8')
-#print(d_login_status)
+
 
 # 로그인 시도
 # os.system("docker login -u sehooh5 -p @Dhtpgh1234")
