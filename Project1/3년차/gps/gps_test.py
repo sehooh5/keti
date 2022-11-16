02c4d471796eb740654d0285d4148e99c42a0e54
sehooh5 <58541635+sehooh5@users.noreply.github.com>
2022-11-16 11:44:41 +0900

    update gps code

--- Project1/3년차/gps/gps_test.py
+++ Project1/3년차/gps/gps_test.py
@@ -2,16 +2,16 @@ import serial
 
 ser = serial.Serial(port='/dev/ttyUSB2', baudrate=9600, parity=serial.PARITY_NONE,
                     stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS)
-# if ser.readable():
-#     res = ser.readline()
-#     print(res.decode()[:len(res) - 1])
+if ser.readable():
+    res = ser.readline()
+    print(res.decode()[:len(res) - 1])
 
-buffer=""
-while True:
-   oneByte = ser.read(1)
-   if oneByte == b"\r":    #byte단위로 read, 구분자 '\r'
-        break
-   else:
-        buffer += oneByte.decode()
-
-print (buffer.strip())
+# buffer=""
+# while True:
+#    oneByte = ser.read(1)
+#    if oneByte == b"\r":    #byte단위로 read, 구분자 '\r'
+#         break
+#    else:
+#         buffer += oneByte.decode()
+#
+# print (buffer.strip())
