c0db4515f9245d59e2c101fbd7e8ecb32f15946a
sehooh5 <58541635+sehooh5@users.noreply.github.com>
2022-11-18 17:02:10 +0900

    update gps

--- Project1/3년차/gps/gps_with_gyro2.cpp
+++ Project1/3년차/gps/gps_with_gyro2.cpp
@@ -472,13 +472,13 @@ void ParseData(char chr)
 		{
 		        case 0x50:
 		            printf("\r\n[[Data Output Start]]\r\n");
-                    yy = get_time(chrBuf, 1);
-                    mm = get_time(chrBuf, 2);
-                    dd = get_time(chrBuf, 3);
-                    hh = get_time(chrBuf, 4);
-                    mi = get_time(chrBuf, 5);
-                    ss = get_time(chrBuf, 6);
-                    ms = get_time(chrBuf, 7);
+                    yy = (unsigned int)get_time(chrBuf, 1);
+                    mm = (unsigned int)get_time(chrBuf, 2);
+                    dd = (unsigned int)get_time(chrBuf, 3);
+                    hh = (unsigned int)get_time(chrBuf, 4);
+                    mi = (unsigned int)get_time(chrBuf, 5);
+                    ss = (unsigned int)get_time(chrBuf, 6);
+                    ms = (unsigned int)get_time(chrBuf, 7);
                     printf("[0x50] %f %f %f %f %f %f %f\r\n", yy,mm,dd,hh,mi,ss,ms);
 		            break;
 				case 0x51:
