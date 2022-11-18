114bc5c823152ff56b5518bc3d4962104fc44fd1
sehooh5 <58541635+sehooh5@users.noreply.github.com>
2022-11-18 15:45:19 +0900

    update gps

--- Project1/3년차/gps/gps_with_gyro2.cpp
+++ Project1/3년차/gps/gps_with_gyro2.cpp
@@ -373,9 +373,9 @@ void ParseData(char chr)
                     lat = get_gpsData(chrBuf,2);
 
                     lon_dd = lon/100000000;
-                    lon_mm = (lon%100000000)/100000;
+                    lon_mm = (((double)lon)%100000000)/100000;
                     lat_dd = lat/100000000;
-                    lat_mm = (lat%100000000)/100000;
+                    lat_mm = (((double)lat)%100000000)/100000;
                     printf("[0x57] lon : %f.%f lat : %f.%f\r\n",lon_dd,lon_mm,lat_dd,lat_mm);
 				    break;
                 case 0x58:
