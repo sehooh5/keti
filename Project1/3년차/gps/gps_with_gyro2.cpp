5ff92f031084f4a89b159bf677a4ff0e2cdf333b
sehooh5 <58541635+sehooh5@users.noreply.github.com>
2022-11-21 14:54:20 +0900

    update gps code

--- Project1/3년차/gps/gps_with_gyro2.cpp
+++ Project1/3년차/gps/gps_with_gyro2.cpp
@@ -308,7 +308,7 @@ float get_magnetic(char *chrBuf, int num)
 //0x56 Atmospheric data 추출
 double get_atmospheric(char *chrBuf, int num)
 {
-    double press; double h;
+    unsigned int press; double h;
     signed int tmp[8];// int로 변경
     unsigned char i;
 
@@ -317,7 +317,7 @@ double get_atmospheric(char *chrBuf, int num)
     }
 
     if (num==1){
-        press = (double)((tmp[3]<<24)|(tmp[2]<<16)|(tmp[1]<<8)|tmp[0]);
+        press = (unsigned int)((tmp[3]<<24)|(tmp[2]<<16)|(tmp[1]<<8)|tmp[0]);
         return press;
     }
     else if (num==2){
@@ -448,7 +448,7 @@ float ax; float ay; float az; float t; //0x51
 float wx; float wy; float wz; //0x52
 float roll; float pitch; float yaw;//0x53
 float mx; float my; float mz;//0x54
-double press; double h; //0x56
+unsigned int press; double h; //0x56
 double lon; double lat; double lon_dd; double lat_dd; double lon_mm; double lat_mm;//0x57
 double gh; double gy; double gv;//0x58
 float q0; float q1; float q2; float q3;//0x59
@@ -512,7 +512,7 @@ void ParseData(char chr)
                 case 0x56:
                     press = get_atmospheric(chrBuf,1);
                     h = get_atmospheric(chrBuf,2);
-                    printf("[0x56] press : %lf h : %lf\r\n", press, h);
+                    printf("[0x56] press : %lx h : %lf\r\n", press, h);
 				    break;
 				case 0x57:
                     lon = get_gpsData(chrBuf,1);
