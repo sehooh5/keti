67e11380f08f97192a7bd61ecf61a2c78161c580
sehooh5 <58541635+sehooh5@users.noreply.github.com>
2022-11-21 14:46:34 +0900

    update gps code

--- Project1/3년차/gps/gps_with_gyro2.cpp
+++ Project1/3년차/gps/gps_with_gyro2.cpp
@@ -511,9 +511,9 @@ void ParseData(char chr)
 				    break;
                 case 0x56:
                     press = get_atmospheric(chrBuf,1);
-                    press2 = (unsigned int)press;
+                    pressure = (unsigned int)press;
                     h = get_atmospheric(chrBuf,2);
-                    printf("[0x56] press : %lx h : %lf\r\n", press2, h);
+                    printf("[0x56] press : %lx h : %lf\r\n", pressure, h);
 				    break;
 				case 0x57:
                     lon = get_gpsData(chrBuf,1);
