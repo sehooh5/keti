942b76513f31a4b27b79c33f8f95e0ad8cca8e00
sehooh5 <58541635+sehooh5@users.noreply.github.com>
2022-11-21 15:54:01 +0900

    update gps code

--- Project1/3년차/gps/gps_with_gyro2.cpp
+++ Project1/3년차/gps/gps_with_gyro2.cpp
@@ -372,7 +372,7 @@ double get_groundSpeed(char *chrBuf, int num)
     }
     else if (num==3){
         gv = ((double)((tmp[7]<<24)|(tmp[6]<<16)|(tmp[5]<<8)|tmp[4]))/1000;
-        print(gv)
+        printf("### %lf",gv);
         return gv;
     }
     else{
