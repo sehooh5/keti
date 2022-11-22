e89feb1bdd821c5fcbd8af7059eda65df5a2724f
sehooh5 <58541635+sehooh5@users.noreply.github.com>
2022-11-22 16:38:34 +0900

    update gps code

--- Project1/3년차/gps/gps_with_gyro3.cpp
+++ Project1/3년차/gps/gps_with_gyro3.cpp
@@ -360,11 +360,11 @@ double get_gpsData(unsigned char *chrBuf, int num)
 float get_groundSpeed(unsigned char *chrBuf, int num)
 {
     float gh; float gy; float gv;
-    signed short tmp[8];
+    unsigned short tmp[8];
     unsigned char i;
 
     for(i=0;i<8;i++){
-        tmp[i] = (signed short)chrBuf[i+2];
+        tmp[i] = (unsigned short)chrBuf[i+2];
     }
 
     if (num==1){
