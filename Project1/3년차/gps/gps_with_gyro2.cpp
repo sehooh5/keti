ea1d66b2d3df7ef7e0958b2242130bfc59eb996c
sehooh5 <58541635+sehooh5@users.noreply.github.com>
2022-11-22 11:03:21 +0900

    update gps code

--- Project1/3년차/gps/gps_with_gyro2.cpp
+++ Project1/3년차/gps/gps_with_gyro2.cpp
@@ -340,13 +340,13 @@ double get_gpsData(char *chrBuf, int num)
     }
 
     if (num==1){
-        lon = (tmp[3]<<24)|(tmp[2]<<16)|(tmp[1]<<8)|tmp[0];
+        lon = ((tmp[3]<<24)|(tmp[2]<<16)|(tmp[1]<<8)|tmp[0]);
 //        lon = (double)((tmp[3]<<24)|(tmp[2]<<16)|(tmp[1]<<8)|tmp[0]);
         printf("\r\n* lon base : %lf\r\n", lon);
         return lon;
     }
     else if (num==2){
-        lat = (tmp[7]<<24)|(tmp[6]<<16)|(tmp[5]<<8)|tmp[4];
+        lat = ((tmp[7]<<24)|(tmp[6]<<16)|(tmp[5]<<8)|tmp[4]);
         printf("* lat base : %lf\r\n\r\n", lat);
         return lat;
     }
