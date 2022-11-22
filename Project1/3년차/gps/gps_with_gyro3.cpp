b205f6108a330f14f1843d018c5176b74b1bb2fd
sehooh5 <58541635+sehooh5@users.noreply.github.com>
2022-11-22 16:57:35 +0900

    update gps code

--- Project1/3년차/gps/gps_with_gyro3.cpp
+++ Project1/3년차/gps/gps_with_gyro3.cpp
@@ -545,8 +545,10 @@ void ParseData(unsigned char chr)
                     lon_dd = (int)(lon/10000000);
                     lat_dd = (int)(lat/10000000);
                     lon_mm = ((lon/10000000-lon_dd)*100)/60;
+                    lat_mm = ((lat/10000000-lat_dd)*100)/60;
+                    lon_final = lon_dd+lon_mm;
                     printf("[0x57] lon : %lf lat : %lf\r\n",lon_dd,lat_dd);
-                    printf("lon _ mm : %lf\n", (double)lon_mm);
+                    printf("lon : %lf\n", (double)lon_final);
 			        memset(chrBuf, 0x00, 2000);
 				    break;
                 case 0x58:
