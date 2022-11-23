f8393a7eb4285a8581d83207c1192a03a3a07c04
sehooh5 <58541635+sehooh5@users.noreply.github.com>
2022-11-23 13:11:20 +0900

    update cpp http test

--- Project1/3년차/gps/cpp_http_test.cpp
+++ Project1/3년차/gps/cpp_http_test.cpp
@@ -1,50 +1,45 @@
+#include <stdio.h>
 #include <curl/curl.h>
-#include <json/json.h>
 
-CURL *curl;
-CURLcode res;
-
-std::string strTargetURL;
-std::string strResourceJSON;
-
-struct curl_slist *headerlist = nullptr;
-headerlist = curl_slist_append(headerlist, "Content-Type: application/json");
-
-strTargetURL = "http://123.214.186.162:8088/test";
-strResourceJSON = "{\"snippet\": {\"title\": \"this is title\", \"scheduledStartTime\": \"2017-05-15\"},\"status\": {\"privacyStatus\": \"private\"}}";
-
-curl_global_init(CURL_GLOBAL_ALL);
-
-curl = curl_easy_init();
-if (curl)
+int main(void)
 {
-    curl_easy_setopt(curl, CURLOPT_URL, strTargetURL.c_str());
-    curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headerlist);
+  CURL *curl;
+  CURLcode res;
 
-    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, false);
-    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, false);
+  /* In windows, this will init the winsock stuff */
+  curl_global_init(CURL_GLOBAL_ALL); // 이 옵션은 thread 메모리 공유에 안전하지 않다. 나는 주석처리함
 
-    curl_easy_setopt(curl, CURLOPT_POST, 1L);
-    curl_easy_setopt(curl, CURLOPT_POSTFIELDS, strResourceJSON.c_str());
+  /* get a curl handle */
+  curl = curl_easy_init();
 
-    // 결과 기록
-    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
-    curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)&chunk);
+  struct curl_slist *list = NULL;
 
-    res = curl_easy_perform(curl);
+  if(curl) {
+    curl_easy_setopt(curl, CURLOPT_URL, "http://123.214.186.162:8088/test"); //webserver ip 주소와 포트번호, flask 대상 router
 
-    curl_easy_cleanup(curl);
-    curl_slist_free_all(headerlist);
+    list = curl_slist_append(list, "Content-Type: application/json"); // content-type 정의 내용 list에 저장
+    curl_easy_setopt(curl, CURLOPT_HTTPHEADER, list); // content-type 설정
 
-    if (res != CURLE_OK)
-    {
-        fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
-        return false;
-    }
+    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 1L); // 값을 false 하면 에러가 떠서 공식 문서 참고함
+    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 1L); // 값을 false 하면 에러가 떠서 공식 문서 참고함
 
-    std::cout << "------------Result" << std::endl;
-    std::cout << chunk.memory << std::endl;
+    curl_easy_setopt(curl, CURLOPT_POST, 1L); //POST option
+    curl_easy_setopt(curl, CURLOPT_POSTFIELDS,"data"); //string의 data라는 내용을 전송 할것이다
 
-    return true;
+    /* Perform the request, res will get the return code */
+    res = curl_easy_perform(curl); // curl 실행 res는 curl 실행후 응답내용이
+    curl_slist_free_all(list); // CURLOPT_HTTPHEADER 와 세트
+
+    /* Check for errors */
+    if(res != CURLE_OK)
+      fprintf(stderr, "curl_easy_perform() failed: %s\n",curl_easy_strerror(res));
+
+    /* always cleanup */
+    curl_easy_cleanup(curl); // curl_easy_init 과 세트
+  }
+  curl_global_cleanup(); // curl_global_init 과 세트
+  return 0;
+
+  }
+
 }
-return false;
