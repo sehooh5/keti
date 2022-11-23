394af905783f60e92b2ba15a87535fc69b9adddf
sehooh5 <58541635+sehooh5@users.noreply.github.com>
2022-11-23 18:24:32 +0900

    update gps code

--- Project1/3년차/gps/rest/gps_server.py
+++ Project1/3년차/gps/rest/gps_server.py
@@ -22,11 +22,13 @@ def index():
 @app.route('/test', methods=['POST'])
 def test():
     json_data = request.get_json(silent=True)
-    # lat=json_data['gps']['lat_dd']+json_data['gps']['lat_mm']
+    lat_dd = json_data['gps']['lat_dd']
+    lat_mm = json_data['gps']['lat_mm']
+    print(lat_dd)
+    print(type(lat_mm))
     # lon = json_data['gps']['lon_dd'] + json_data['gps']['lon_mm']
-    #
+
     # print(f"lon : {lon}, lat : {lat}")
-    print(json_data)
     return "good"
 
 @app.route('/gps', methods=['POST'])
