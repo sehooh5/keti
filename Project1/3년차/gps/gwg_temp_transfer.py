6edbc63ed007dfaa9b5a58e07beca932adae632b
sehooh5 <58541635+sehooh5@users.noreply.github.com>
2022-11-24 16:06:09 +0900

    update gps code

--- Project1/3년차/gps/gwg_temp_transfer.py
+++ Project1/3년차/gps/gwg_temp_transfer.py
@@ -10,13 +10,14 @@ c = conn.cursor()
 c.execute("SELECT COUNT(*) FROM gwg_save")
 for row in c:
     num = row[0]
-print(num)
+
 while True:
     for num in range(1, num):
         if num == 1:
             print("데이터 초기화")
         c.execute(f"SELECT * FROM gwg_save WHERE ROWID={num}")
         for row in c:
+            print(num,"\n")
             print(row[0], row[1], row[2], row[3], row[4], row[5])
                 # data = {
                 #     "cid": cid,
