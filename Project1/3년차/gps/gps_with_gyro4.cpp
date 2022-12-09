254a6b51ae2d0ded0e3071542dede3962160651b
sehooh5 <58541635+sehooh5@users.noreply.github.com>
2022-12-09 12:00:53 +0900

    update c++ to python

--- Project1/3년차/gps/gps_with_gyro4.cpp
+++ Project1/3년차/gps/gps_with_gyro4.cpp
@@ -487,7 +487,7 @@ extern "C"
     static unsigned char chrBuf[2000];// 밖에서 unsigned char 변수 설정
 
     // Parsing Data
-    void ParseData(unsigned char chr) // 1208 void -> float 으로 변경
+    float ParseData(unsigned char chr) // 1208 void -> float 으로 변경
     {
             static unsigned char chrCnt=0;
             unsigned char i;
