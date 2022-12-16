9e9eede5b3a2f2eb81b9176a7673db5b3692e4fd
sehooh5 <58541635+sehooh5@users.noreply.github.com>
2022-12-16 17:45:02 +0900

    update parsing struct c to python

--- Project1/3년차/gps/gps_with_gyro4.py
+++ Project1/3년차/gps/gps_with_gyro4.py
@@ -3,7 +3,6 @@ import ctypes
 
 path = "./gwg4.so"
 c_module = ctypes.cdll.LoadLibrary(path)
-print("시작!!")
 
 class STRUCT(ctypes.Structure) :
     _fields_ = [("mi", ctypes.c_uint),
@@ -13,5 +12,6 @@ class STRUCT(ctypes.Structure) :
 
 str = STRUCT()
 c_module.process(ctypes.byref(str))
-print("프린트!!!",str.mi)
+print("프린트!!!!!")
+print(str.mi)
 # print(str.mi, str.ss, str.ms, str.ax, str.ay, str.az)
