4b646ddca41ec4d08ec46a3c4aeca9496019b9a8
sehooh5 <58541635+sehooh5@users.noreply.github.com>
2022-12-16 10:46:37 +0900

    udpate c++ struct to python

--- Project1/3년차/gps/gps_with_gyro5.cpp
+++ Project1/3년차/gps/gps_with_gyro5.cpp
@@ -15,10 +15,7 @@
 
 /* c++ to python 추가내용 */
 #define EXPORT
-#include <vector>
-#include <numeric>
-#include <tuple>
-/* end */
+
 
 
 
@@ -33,73 +30,10 @@ extern "C" {
         *((St*)st) = temp;
     }
 
-//__declspec(dllexport)
 }
 
 
-extern "C"
-{
-//    EXPORT void sub(double b, double* result)
-//    {
-//        *result = 12.3;
-//    }
-//
-//
-//    EXPORT typedef struct {
-//        int *a;
-//        int *b;
-//        float *c;
-////        float c;
-////        unsigned char* pBuffer;
-//    }  POINT;
-//
-//    void process(POINT p){
-//        p = {101,20,3.14};
-//
-//        *result = p;
-//    }
 
-
-
-
-
-//    struct POINT input_p(void);
-//    void display_p(struct POINT d);
-//
-//    EXPORT int main(void){
-//        POINT d;
-//        d = input_p();
-//        display_p(d);
-//        return 0;
-//    }
-//
-//    void display_p(struct POINT s){
-//        printf("a %d", s.a);
-//        printf("b %d", s.b);
-//        printf("c %f", s.c);
-//    }
-//
-//    struct POINT input_p(void){
-//        POINT s;
-//        s.a = 10;
-//        s.b = 20;
-//        s.c = 3.14;
-//        return s;
-//    }
-
-
-//    POINT main(void)
-
-
-//    tuple<int, int, float> t1;
-//    EXPORT tuple<int,int,float> getTuple(){
-//        int a = 10;
-//        int b = 20;
-//        float c = 3.14;
-//        return make_tuple(a,b,c);
-//    }
-
-}
 
 
 
--- Project1/3년차/gps/gps_with_gyro5.py
+++ Project1/3년차/gps/gps_with_gyro5.py
@@ -1,9 +1,4 @@
-
-
 import ctypes  # 파이썬 extension을 사용하기 위한 모듈
-
-
-#######################################
 
 
 path = "./gwg5.so"
@@ -19,58 +14,3 @@ obj = Obj()
 lib.func(ctypes.byref(obj))
 print(obj.nNum, obj.fFloat)
 
-
-# ### Pointer 전달해서 받은 값
-# sub = c.sub
-# sub.argtypes = (ctypes.c_double, ctypes.POINTER(ctypes.c_double))
-# sub.restype = None
-# outparam = ctypes.c_double()
-#
-# sub(0, outparam)
-# print(outparam.value)
-#
-#
-#
-# class str(ctypes.Structure):
-#     _fields_ = [
-#         ("a", ctypes.POINTER(ctypes.c_int)),
-#         ("b", ctypes.POINTER(ctypes.c_int)),
-#         ("c", ctypes.POINTER(ctypes.c_float))
-#         # ("p", ctypes.POINTER(c.c_ubyte))
-#     ]
-#
-# res = c.process
-# res.argtypes = ctypes.POINTER(str),
-# res.restype = None
-#
-# s = str(0, ctypes.c_int, ctypes.c_int, ctypes.c_float)
-# result = res(s)
-# print(result)
-
-
-
-
-
-
-# point = POINT()
-# list = c.process(point)
-# print(str(list.a))
-
-
-# tuple = c.getTuple()
-# print(tuple)
-# point = POINT(c.one.a, c.one.b, c.one.c)
-#
-# print(point.a, point.a, point.a)
-
-
-
-# 가장 기본되는 구조체
-# class POINT(ctypes.Structure):
-#     _fields_ = [
-#         ("a", ctypes.c_int),
-#         ("b", ctypes.c_int)
-#     ]
-#
-# point = POINT(10,20)
-# print(point.a, point.b)
