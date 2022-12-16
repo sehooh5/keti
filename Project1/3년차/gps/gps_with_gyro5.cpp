cf97a0f1bed477f3c56a920698c75d107f139e57
sehooh5 <58541635+sehooh5@users.noreply.github.com>
2022-12-16 14:36:45 +0900

    update parsing struct c to python

--- Project1/3년차/gps/gps_with_gyro5.cpp
+++ Project1/3년차/gps/gps_with_gyro5.cpp
@@ -27,7 +27,7 @@ extern "C" {
         // 포인터 지정하는 부분
         // 함수의 포인트 파라미터로 받은 void* st를 선언한 구조체인 Struct*로 형변환 후 *포인터..?
         *((Struct*)st) = temp;
-        std::cout << temp;
+        std::cout << *temp;
     }
 
 }
