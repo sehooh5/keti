03fd2392d5bed15c9b6f2f08456459524b63dddf
sehooh5 <58541635+sehooh5@users.noreply.github.com>
2022-12-16 12:21:09 +0900

    udpate c++ struct to python

--- Project1/3년차/gps/gps_with_gyro5.cpp
+++ Project1/3년차/gps/gps_with_gyro5.cpp
@@ -1,23 +1,28 @@
-
-
-/* c++ to python 추가내용 */
 #define EXPORT
 
-
+// Structure(구조체)에 담을 변수 선언
 int a;
 double b;
 
-struct St {
-	int x;
+// 구조체 선언 - 여기서는 int 와 double 두 개로 이루어진 구조체
+struct Struct {
+	int num;
 	double f;
 };
 
 extern "C" {
-    EXPORT void func(void* st) {
+    // return 값 없이 Pointer로 구조체를 반환하는 함수 정의 및 EXPORT
+    // 함수의 파라미터도 void 로 받으며 Pointer 로 받는다
+    EXPORT void strToPython(void* st) {
+        // 변수 입력
         a = 1991;
         b = 2.8;
-        St temp = {a,b};
-        *((St*)st) = temp;
+
+        // 위에서 선언한 구조체를 변수로 만들고 위에서 입력된 변수들을 구조체에 담는다
+        Struct temp = {a,b};
+        // 포인터 지정하는 부분
+        // 함수의 포인트 파라미터로 받은 void* st를 선언한 구조체인 Struct*로 형변환 후 *포인터..?
+        ((Struct*)st) = temp;
     }
 
 }
--- Project1/3년차/gps/gps_with_gyro5.py
+++ Project1/3년차/gps/gps_with_gyro5.py
@@ -2,15 +2,15 @@ import ctypes  # 파이썬 extension을 사용하기 위한 모듈
 
 
 path = "./gwg5.so"
-lib = ctypes.cdll.LoadLibrary(path)
+c_module = ctypes.cdll.LoadLibrary(path) # cpp 로 생성한 모듈(.so)을 ctypes 모듈로 불러오기
 
-
-class Obj(ctypes.Structure) :
+# .so 에 정의된 구조체와 같은 구조체 만들기
+class STRUCT(ctypes.Structure) :
     _fields_ = [("nNum", ctypes.c_int),
                  ("fFloat", ctypes.c_double)]
 
 
-obj = Obj()
-lib.func(ctypes.byref(obj))
-print(obj.nNum, obj.fFloat)
+str = STRUCT() # 만들어진 구조체 변수선언
+c_module.strToPython(ctypes.byref(str)) # ctypes의 byref 에 구조체를 넣어서 .so 모듈의 함수에 전달
+print(str.nNum, str.fFloat) # 포인터로 반환된 구조체의 변수 불러오기
 
