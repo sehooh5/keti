085c9ed3618c1635caeaaead6ee75d2c671dca64
sehooh5 <58541635+sehooh5@users.noreply.github.com>
2023-04-05 12:27:10 +0900

    update segmentation error

--- Project1/3rd/gps/gwg/gps_with_gyro_gpt.py
+++ Project1/3rd/gps/gwg/gps_with_gyro_gpt.py
@@ -35,23 +35,12 @@ def process_data():
             print(e)
             traceback.print_exc()
 
-        data = Data(
-            time={"yy": str.yy, "mm": str.mm, "dd": str.dd, "hh": str.hh, "mi": str.mi, "ss": str.ss, "ms": str.ms},
-            acc={"ax": str.ax, "ay": str.ay, "az": str.az},
-            angular={"wx": str.wx, "wy": str.wy, "wz": str.wz},
-            angle={"roll": str.roll, "pitch": str.pitch, "yaw": str.yaw},
-            magnetic={"mx": str.mx, "my": str.my, "mz": str.mz},
-            atmospheric={"press": str.press, "h": str.h},
-            gps={"lat_final": str.lat_final, "lon_final": str.lon_final},
-            groundSpeed={"gh": str.gh, "gy": str.gy, "gv": str.gv},
-            quaternion={"q0": str.q0, "q1": str.q1, "q2": str.q2, "q3": str.q3},
-            satelite={"snum": str.sn, "pdop": str.pdop, "hdop": str.hdop, "vdop": str.vdop}
-        )
 
-        print(data)
+
+        print(str.__dict__)
 
         with requests.Session() as session:
-            json_data = json.dumps(data._asdict())
+            json_data = json.dumps(str.__dict__)
             arg = sys.argv
             if len(arg) > 1:
                 session.post(f'{url}/cgwg_save', json=json_data)
