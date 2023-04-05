4ce549a8920b2afea4ac560928f8832f17ef7912
sehooh5 <58541635+sehooh5@users.noreply.github.com>
2023-04-05 12:18:01 +0900

    update segmentation error

---
+++ Project1/3rd/gps/gwg/gps_with_gyro(gpt).py
@@ -0,0 +1,54 @@
+import ctypes
+import traceback
+import sys
+
+import threading
+import json
+import requests
+
+from collections import namedtuple
+
+url = "http://123.214.186.162:8088"
+path = "./gwg.so"
+c_module = ctypes.cdll.LoadLibrary(path)
+
+Data = namedtuple('Data', ['time', 'acc', 'angular', 'angle', 'magnetic', 'atmospheric', 'gps', 'groundSpeed', 'quaternion', 'satelite'])
+
+def process_data():
+    str = c_module.Struct()
+    while True:
+        try:
+            c_module.process(ctypes.byref(str))
+        except Exception as e:
+            print(e)
+            traceback.print_exc()
+
+        data = Data(
+            time={"yy": str.yy, "mm": str.mm, "dd": str.dd, "hh": str.hh, "mi": str.mi, "ss": str.ss, "ms": str.ms},
+            acc={"ax": str.ax, "ay": str.ay, "az": str.az},
+            angular={"wx": str.wx, "wy": str.wy, "wz": str.wz},
+            angle={"roll": str.roll, "pitch": str.pitch, "yaw": str.yaw},
+            magnetic={"mx": str.mx, "my": str.my, "mz": str.mz},
+            atmospheric={"press": str.press, "h": str.h},
+            gps={"lat_final": str.lat_final, "lon_final": str.lon_final},
+            groundSpeed={"gh": str.gh, "gy": str.gy, "gv": str.gv},
+            quaternion={"q0": str.q0, "q1": str.q1, "q2": str.q2, "q3": str.q3},
+            satelite={"snum": str.sn, "pdop": str.pdop, "hdop": str.hdop, "vdop": str.vdop}
+        )
+
+        print(data)
+
+        with requests.Session() as session:
+            json_data = json.dumps(data._asdict())
+            arg = sys.argv
+            if len(arg) > 1:
+                session.post(f'{url}/cgwg_save', json=json_data)
+            else:
+                session.post(f'{url}/cgwg', json=json_data)
+
+def main():
+    t = threading.Thread(target=process_data)
+    t.start()
+
+if __name__ == '__main__':
+    main()
--- Project1/3rd/gps/gwg/gps_with_gyro.py
+++ Project1/3rd/gps/gwg/gps_with_gyro.py
@@ -1,54 +1,72 @@
 import ctypes
+import requests
 import traceback
+import json
 import sys
 
-import threading
-import json
-import requests
-
-from collections import namedtuple
-
 url = "http://123.214.186.162:8088"
+
 path = "./gwg.so"
 c_module = ctypes.cdll.LoadLibrary(path)
+# Global 변수 설정
+global ss, ms
 
-Data = namedtuple('Data', ['time', 'acc', 'angular', 'angle', 'magnetic', 'atmospheric', 'gps', 'groundSpeed', 'quaternion', 'satelite'])
+# def req_post(url):
+#     print(f'Request POST to : {url}')
+#     print(ss, ms)
+#     res = requests.get(f'{url}/cgwg')
+#     return res
 
-def process_data():
-    str = c_module.Struct()
-    while True:
-        try:
-            c_module.process(ctypes.byref(str))
-        except Exception as e:
-            print(e)
-            traceback.print_exc()
+class STRUCT(ctypes.Structure) :
+    _fields_ = [("yy", ctypes.c_uint),("mm", ctypes.c_uint),("dd", ctypes.c_uint),("hh", ctypes.c_uint),("mi", ctypes.c_uint),("ss", ctypes.c_uint),("ms", ctypes.c_uint),
+                ("ax", ctypes.c_float),("ay", ctypes.c_float),("az", ctypes.c_float),("t", ctypes.c_float),
+                ("wx", ctypes.c_float), ("wy", ctypes.c_float), ("wz", ctypes.c_float),
+                ("roll", ctypes.c_float), ("pitch", ctypes.c_float), ("yaw", ctypes.c_float),
+                ("mx", ctypes.c_float), ("my", ctypes.c_float), ("mz", ctypes.c_float),
+                ("press", ctypes.c_float), ("h", ctypes.c_float),
+                ("lon_final", ctypes.c_double), ("lat_final", ctypes.c_double),
+                ("gh", ctypes.c_float), ("gy", ctypes.c_float), ("gv", ctypes.c_float),
+                ("q0", ctypes.c_float), ("q1", ctypes.c_float), ("q2", ctypes.c_float),("q3", ctypes.c_float),
+                ("sn", ctypes.c_float), ("pdop", ctypes.c_float), ("hdop", ctypes.c_float), ("vdop", ctypes.c_float)
+                ]
 
-        data = Data(
-            time={"yy": str.yy, "mm": str.mm, "dd": str.dd, "hh": str.hh, "mi": str.mi, "ss": str.ss, "ms": str.ms},
-            acc={"ax": str.ax, "ay": str.ay, "az": str.az},
-            angular={"wx": str.wx, "wy": str.wy, "wz": str.wz},
-            angle={"roll": str.roll, "pitch": str.pitch, "yaw": str.yaw},
-            magnetic={"mx": str.mx, "my": str.my, "mz": str.mz},
-            atmospheric={"press": str.press, "h": str.h},
-            gps={"lat_final": str.lat_final, "lon_final": str.lon_final},
-            groundSpeed={"gh": str.gh, "gy": str.gy, "gv": str.gv},
-            quaternion={"q0": str.q0, "q1": str.q1, "q2": str.q2, "q3": str.q3},
-            satelite={"snum": str.sn, "pdop": str.pdop, "hdop": str.hdop, "vdop": str.vdop}
-        )
 
-        print(data)
+str = STRUCT()
+while True:
+    # time.sleep(0.5)
+    try:
+        print("cpp 라이브러리 호출!")
+        c_module.process(ctypes.pointer(str)) # byref # 여기 C 과정에서 세그멘테이션 오류 발생
+    except Exception as e:
+        print(e)
+        traceback.print_exc()
+    print("데이터 출력 : ")
 
-        with requests.Session() as session:
-            json_data = json.dumps(data._asdict())
-            arg = sys.argv
-            if len(arg) > 1:
-                session.post(f'{url}/cgwg_save', json=json_data)
-            else:
-                session.post(f'{url}/cgwg', json=json_data)
+    ss = str.ss
+    ms = str.ms
+    data = {
+        "time":{"yy": str.yy,"mm": str.mm,"dd": str.dd,"hh": str.hh,"mi": str.mi,"ss": str.ss,"ms": str.ms},
+        "acc":{"ax":str.ax,"ay":str.ay,"az":str.az},
+        "angular":{"wx":str.wx,"wy":str.wy,"wz":str.wz},
+        "angle":{"roll":str.roll,"pitch":str.pitch,"yaw":str.yaw},
+        "magnetic":{"mx": str.mx, "my": str.my, "mz": str.mz},
+        "atmospheric":{"press": str.press, "h": str.h},
+        "gps":{"lat_final": str.lat_final, "lon_final": str.lon_final},
+        "groundSpeed":{"gh": str.gh, "gy": str.gy, "gv": str.gv},
+        "quaternion":{"q0": str.q0, "q1": str.q1, "q2": str.q2, "q3": str.q3},
+        "satelite":{"snum": str.sn, "pdop": str.pdop, "hdop": str.hdop, "vdop": str.vdop},
+    }
+    print(data)
+    json_data = json.dumps(data)
+    arg = sys.argv
 
-def main():
-    t = threading.Thread(target=process_data)
-    t.start()
+    if len(arg) > 1 :
+        requests.post(f'{url}/cgwg_save', json=json_data)
+    else:
+        requests.post(f'{url}/cgwg', json=json_data)
 
-if __name__ == '__main__':
-    main()
+    # time.sleep(0.5)
+
+    # res = req_post(url)
+    # print(res)
+
