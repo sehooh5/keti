e97a93fc4de0ca3fd765f892465c74f8c63dc4a4
sehooh5 <58541635+sehooh5@users.noreply.github.com>
2023-04-05 12:25:10 +0900

    update segmentation error

--- Project1/3rd/gps/gwg/gps_with_gyro_gpt.py
+++ Project1/3rd/gps/gwg/gps_with_gyro_gpt.py
@@ -12,10 +12,22 @@ url = "http://123.214.186.162:8088"
 path = "./gwg.so"
 c_module = ctypes.cdll.LoadLibrary(path)
 
-Data = namedtuple('Data', ['time', 'acc', 'angular', 'angle', 'magnetic', 'atmospheric', 'gps', 'groundSpeed', 'quaternion', 'satelite'])
+class Struct:
+    def __init__(self):
+        self.time = {"yy": 0, "mm": 0, "dd": 0, "hh": 0, "mi": 0, "ss": 0, "ms": 0}
+        self.acc = {"ax": 0.0, "ay": 0.0, "az": 0.0}
+        self.angular = {"wx": 0.0, "wy": 0.0, "wz": 0.0}
+        self.angle = {"roll": 0.0, "pitch": 0.0, "yaw": 0.0}
+        self.magnetic = {"mx": 0.0, "my": 0.0, "mz": 0.0}
+        self.atmospheric = {"press": 0.0, "h": 0.0}
+        self.gps = {"lat_final": 0.0, "lon_final": 0.0}
+        self.groundSpeed = {"gh": 0.0, "gy": 0.0, "gv": 0.0}
+        self.quaternion = {"q0": 0.0, "q1": 0.0, "q2": 0.0, "q3": 0.0}
+        self.satelite = {"snum": 0.0, "pdop": 0.0, "hdop": 0.0, "vdop": 0.0}
 
+str = Struct()
 def process_data():
-    str = Struct()
+
     while True:
         try:
             c_module.process(ctypes.byref(str))
