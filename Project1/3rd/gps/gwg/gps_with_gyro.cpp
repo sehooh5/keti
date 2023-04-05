4c6211adc8572613815eedf6039707beb4861aaf
sehooh5 <58541635+sehooh5@users.noreply.github.com>
2023-04-05 12:04:55 +0900

    update segmentation error

--- Project1/3rd/gps/gwg/gps_with_gyro.cpp
+++ Project1/3rd/gps/gwg/gps_with_gyro.cpp
@@ -700,8 +700,10 @@ extern "C"
             }
             // 1216 struct 값 입력 및 반환
             printf("구조체 전달!\n");
-            Struct* temp = new Struct{yy,mm,dd,hh,mi,ss,ms,ax,ay,az,t,wx,wy,wz,roll,pitch,yaw,mx,my,mz,press,h,lon_final,lat_final,gh,gy,gv,q0,q1,q2,q3,sn,pdop,hdop,vdop};
-            *((Struct*)st) = *temp;
+            if (st != NULL) { // 추가된 NULL 체크
+                Struct temp = { yy, mm, dd, hh, mi, ss, ms, ax, ay, az, t, wx, wy, wz, roll, pitch, yaw, mx, my, mz, press, h, lon_final, lat_final, gh, gy, gv, q0, q1, q2, q3, sn, pdop, hdop, vdop };
+                *((Struct*)st) = temp;
+            }
             delete temp;
         } catch (std::exception& e) {
             std::cerr << "Exception caught: " << e.what() << std::endl;
