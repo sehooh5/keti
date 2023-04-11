2fc7d0d6296651d0ccff794e55eae4f4c41f86c3
sehooh5 <58541635+sehooh5@users.noreply.github.com>
2023-04-11 17:31:20 +0900

    update blackbox_gui

--- Project1/3rd/gps/blackbox_GUI/app_GUI.py
+++ Project1/3rd/gps/blackbox_GUI/app_GUI.py
@@ -49,16 +49,19 @@ class App(QWidget):
 
         # 입력창 1
         input1 = QLineEdit(self)
+        input1.setPlaceholderText('카메라 주소 입력')
         input1.move(50, 220)
         input1.resize(300, 25)
 
         # 입력창 2
         input2 = QLineEdit(self)
+        input2.setPlaceholderText('엣지서버 주소')
         input2.move(50, 260)
         input2.resize(300, 25)
 
         # 입력창 3
         input3 = QLineEdit(self)
+        input3.setPlaceholderText('엣지서버 포트번호')
         input3.move(50, 300)
         input3.resize(300, 25)
 
@@ -66,13 +69,13 @@ class App(QWidget):
         btn1 = QPushButton('실행', self)
         btn1.setToolTip('VLC 실행')
         btn1.move(50, 340)
-        btn1.clicked.connect(lambda: self.start_process(input1.text(), input2.text(), input3.text()))
+        btn1.clicked.connect(lambda: self.start_process2(input1.text(), input2.text(), input3.text()))
 
         # 멈춤 버튼
         btn2 = QPushButton('멈춤', self)
         btn2.setToolTip('VLC 종료')
         btn2.move(150, 340)
-        btn2.clicked.connect(self.stop_process)
+        btn2.clicked.connect(self.stop_process2)
         self.show()
 
     def start_process(self):
@@ -86,6 +89,18 @@ class App(QWidget):
             self.process = subprocess.Popen(['python3', 'gps_with_gyro.py', 'save'])
 
     def stop_process(self):
+        # 실행 중인 프로세스가 있는 경우에만 종료
+        if self.process is not None and self.process.poll() is None:
+            self.process.terminate()
+            self.process.wait()
+
+    def start_process2(self, input1, input2, input3):
+    # 실행 중인 프로세스가 없는 경우에만 실행
+    if self.process is None or self.process.poll() is not None:
+        command = ['cvlc', '-vvv', input1, '--sout="#rtp{dst=' + input2 + ',port=' + input3 + ',mux=ts}"', '--no-sout-all', '--sout-keep']
+        self.process = subprocess.Popen(command)
+
+    def stop_process2(self):
         # 실행 중인 프로세스가 있는 경우에만 종료
         if self.process is not None and self.process.poll() is None:
             self.process.terminate()
