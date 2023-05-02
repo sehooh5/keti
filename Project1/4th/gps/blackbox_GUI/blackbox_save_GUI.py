3ac3f5b6b5222d0b56b6c152789d23a3a3462afb
sehooh5 <58541635+sehooh5@users.noreply.github.com>
2023-05-02 14:57:55 +0900

    update gps save code

--- Project1/4th/gps/blackbox_GUI/blackbox_save_GUI.py
+++ Project1/4th/gps/blackbox_GUI/blackbox_save_GUI.py
@@ -136,7 +136,8 @@ class App(QWidget):
         # 실행 중인 프로세스가 없는 경우에만 실행
         if self.process2_thread is None or not self.process2_thread.isRunning():
 #             command = 'cvlc rtsp://192.168.1.101:554/h264 --sout=file/mp4:/home/keti-laptop/비디오/{}'.format(input1)
-            command = 'cvlc rtsp://192.168.1.101:554/h264 --sout=file/mp4:/home/keti-laptop/비디오/test.mp4'
+            command = 'cvlc rtsp://192.168.1.101:554/h264 --sout=file/avi:/home/keti-laptop/비디오/test.avi
+'
             self.process2_thread = subprocess.Popen(command, shell=True)
             self.status3.setText('영상 저장중')
 
