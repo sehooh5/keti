d864b910af24d4f087619ff81299c4f8686a4077
sehooh5 <58541635+sehooh5@users.noreply.github.com>
2023-05-02 17:31:25 +0900

    update gps save code

--- Project1/4th/gps/gwg_server.py
+++ Project1/4th/gps/gwg_server.py
@@ -178,7 +178,7 @@ def cgwg_save():
 #34
 
     c.execute(f"INSERT INTO {dbname} \
-                VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)", (yy, mm, dd, hh, mi, ss, ms, ax, ay, az, wx, wy, wz, roll, pitch, yaw, mx, my, mz, press, h, lat, lon, gh, gy, gv, q0, q1, q2, q3, snum, pdop, hdop, vdop, datetime.datetime.now()))
+                VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)", (yy, mm, dd, hh, mi, ss, ms, ax, ay, az, wx, wy, wz, roll, pitch, yaw, mx, my, mz, press, h, lat, lon, gh, gy, gv, q0, q1, q2, q3, snum, pdop, hdop, vdop, datetime.datetime.now()))
 
     return "GPS and Gyro data is saved in Edge Server!"
 
