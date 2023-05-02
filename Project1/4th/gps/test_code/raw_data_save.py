68fae2c79bd12c5e7e93db3429b98c341c81f643
sehooh5 <58541635+sehooh5@users.noreply.github.com>
2023-05-02 15:11:12 +0900

    update gps save code

--- Project1/4th/gps/test_code/raw_data_save.py
+++ Project1/4th/gps/test_code/raw_data_save.py
@@ -12,7 +12,19 @@ for port in ports:
 ser = serial.Serial(port, 9600, timeout=1)
 
 # GPS 데이터를 저장할 파일 열기
-with open('gps_data.txt', 'w') as file:
-    while True:
-            line = ser.readline().decode("utf-8")
-            file.write(line)
+with open('gps_data.txt', 'wb') as file:
+
+        # 무한 루프
+        while True:
+
+            # 시리얼 데이터 읽기
+            data = ser.readline()
+
+            # 데이터가 존재하는 경우에만 처리
+            if data:
+
+                # 데이터 출력
+                print(data)
+
+                # 데이터 파일에 쓰기
+                file.write(data)
