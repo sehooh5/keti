225feef93396c428ad8e08282d7f9ee52b374f75
sehooh5 <58541635+sehooh5@users.noreply.github.com>
2023-05-16 17:39:23 +0900

    sending rtp video from osan data

--- Project1/4th/GPSDataCast/gpsdatacast.py
+++ Project1/4th/GPSDataCast/gpsdatacast.py
@@ -47,39 +47,41 @@ class App(QWidget):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
 
-        # GPS 데이터
         # 제목
         title = QLabel('영상 및 GPS 데이터 전송', self)
         title.move(95, 10)
-        for num in range(1,8):
-            # 영상 RTP 전송
 
-            status = QLabel(f'blackbox_0{num} 전송 멈춤', self)
-            setattr(self, f'status{num}', status)
-            status.move(50, 50*num)
+        # 1번 영상 전송
+        self.status1 = QLabel('blackbox_01 전송 멈춤', self)
+        self.status1.move(50, 50)
 
-            # 영상 RTP 전송 버튼
-            start = QPushButton('전송', self)
-            setattr(self, f'start{num}', self)
-            start.setToolTip('blackbox_01 RTP 전송')
-            start.move(220, 50*num)
-            start.clicked.connect(lambda: self.start_process(num))
-#             start1 = QPushButton('전송', self)
-#             start1.setToolTip('blackbox_01 RTP 전송')
-#             start1.move(220, 50)
-#             start1.clicked.connect(lambda: self.start_process(1))
+        # 1번 영상 RTP 전송 버튼
+        start1 = QPushButton('전송', self)
+        start1.setToolTip('blackbox_01 RTP 전송')
+        start1.move(220, 50)
+        start1.clicked.connect(lambda: self.start_process(1))
 
-            # 영상 멈춤 버튼
-            stop = QPushButton('멈춤', self)
-            setattr(self, f'stop{num}', self)
-            stop.setToolTip('blackbox_01 RTP 전송 멈춤')
-            stop.move(305, 50*num)
-            stop.clicked.connect(lambda: self.stop_process(num))
-#             stop1 = QPushButton('멈춤', self)
-#             stop1.setToolTip('blackbox_01 RTP 전송 멈춤')
-#             stop1.move(305, 50)
-#             stop1.clicked.connect(lambda: self.stop_process(1))
+        # 1번 영상 멈춤 버튼
+        stop1 = QPushButton('멈춤', self)
+        stop1.setToolTip('blackbox_01 RTP 전송 멈춤')
+        stop1.move(305, 50)
+        stop1.clicked.connect(lambda: self.stop_process(1))
 
+        # 2번 영상 전송
+        self.status2 = QLabel('blackbox_02 전송 멈춤', self)
+        self.status2.move(50, 100)
+
+        # 2번 영상 RTP 전송 버튼
+        start2 = QPushButton('전송', self)
+        start2.setToolTip('blackbox_02 RTP 전송')
+        start2.move(220, 100)
+        start2.clicked.connect(lambda: self.start_process(2))
+
+        # 2번 영상 멈춤 버튼
+        stop2 = QPushButton('멈춤', self)
+        stop2.setToolTip('blackbox_02 RTP 전송 멈춤')
+        stop2.move(305, 100)
+        stop2.clicked.connect(lambda: self.stop_process(2))
 
         self.show()
 
@@ -96,7 +98,7 @@ class App(QWidget):
 
     def stop_process(self, num):
         # 실행 중인 프로세스가 있는 경우에만 종료
-        print("rtp 전송 멈춤")
+        print(f"blackbox_0{num} rtp 전송 멈춤")
         process_thread = getattr(self, f"process{num}_thread")
         if process_thread is not None:
             for child in psutil.Process(process_thread.pid).children(recursive=True):
