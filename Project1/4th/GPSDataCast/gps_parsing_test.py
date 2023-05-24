1ed19e13728cce396f39c7de07e45c5b109e40ef
sehooh5 <58541635+sehooh5@users.noreply.github.com>
2023-05-24 17:52:50 +0900

    update gps parsing test code

--- Project1/4th/GPSDataCast/gps_parsing_test.py
+++ Project1/4th/GPSDataCast/gps_parsing_test.py
@@ -22,7 +22,8 @@ while True:
             print("데이터 초기화")
         c.execute(f"SELECT * FROM gps_raw_data WHERE ROWID={num}")
         for row in c:
-            print(f"로우 데이터 {row[0]}: {row[2]}")
+            decoded_data = bytes.fromhex(row[2])
+            print(f"로우 데이터 {row[0]}: {decoded_data}")
 
 
 
