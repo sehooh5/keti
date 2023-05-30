49de959f1cfb5ccea91ab1bd3a3cee2d91094a7b
sehooh5 <58541635+sehooh5@users.noreply.github.com>
2023-05-30 17:07:41 +0900

    update gps parsing test code

--- Project1/4th/GPSDataCast/gps_parsing_test.py
+++ Project1/4th/GPSDataCast/gps_parsing_test.py
@@ -38,8 +38,7 @@ params = {
 while True:
     res = requests.get(url, params)
     raw_data = res.json()
-    print(raw_data)
-#     print(raw_data['raw_data'], " : ", type(raw_data['raw_data']))
+    print(raw_data['gps_raw_data'], " : ", type(raw_data['gps_raw_data']))
     time.sleep(0.5)
 
 
