b6ccbe6cc7bc0db172f7016ae423f4c70d2868cf
sehooh5 <58541635+sehooh5@users.noreply.github.com>
2023-06-19 12:52:43 +0900

    update get resource

--- Project6/resource_check/get_resource.py
+++ Project6/resource_check/get_resource.py
@@ -3,15 +3,9 @@ import subprocess
 
 if sys.argv[1] == 'd':
     print("docker resources check")
-    output = subprocess.check_output(
-        "docker stats",
-        stderr=subprocess.STDOUT,
-        shell=True)
+    output = subprocess.check_output("docker stats",shell=True)
     print(output)
 elif sys.argv[1] == 'k':
     print("k8s resources check")
-    output = subprocess.check_output(
-        "kubectl top pod",
-        stderr=subprocess.STDOUT,
-        shell=True)
+    output = subprocess.check_output("kubectl top pod",shell=True)
     print(output)
