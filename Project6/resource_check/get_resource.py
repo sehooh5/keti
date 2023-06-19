aeb44d578eba9ac29697f1b364972cf5ef5c4dfd
sehooh5 <58541635+sehooh5@users.noreply.github.com>
2023-06-19 12:59:46 +0900

    update get resource

--- Project6/resource_check/get_resource.py
+++ Project6/resource_check/get_resource.py
@@ -3,7 +3,7 @@ import subprocess
 
 if sys.argv[1] == 'd':
     print("docker resources check")
-    output = subprocess.check_output("docker stats",shell=True)
+    output = subprocess.check_output("docker stats --no-stream",shell=True)
     print(output)
 elif sys.argv[1] == 'kp':
     print("k8s pod resources check")
@@ -13,3 +13,5 @@ elif sys.argv[1] == 'kn':
     print("k8s node resources check")
     output = subprocess.check_output("kubectl top no",shell=True)
     print(output)
+else:
+    print("argument not available")
