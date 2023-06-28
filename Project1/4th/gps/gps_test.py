146722bce69904911f48bf2778f9583da960c4b8
sehooh5 <58541635+sehooh5@users.noreply.github.com>
2023-06-28 14:24:58 +0900

    update daily

--- Project1/4th/gps/gps_test.py
+++ Project1/4th/gps/gps_test.py
@@ -1,18 +1,8 @@
-import serial
+import sqlite3
 
-ser = serial.Serial(port='/dev/ttyUSB2', baudrate=9600, parity=serial.PARITY_NONE,
-                    stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS)
-# if ser.readable():
-#     res = ser.readline()
-#     print(res.decode('utf-16')[:len(res) - 1])
-
-buffer=""
-while True:
-   oneByte = ser.read(2)
-   if oneByte == b"\r":    #byte단위로 read, 구분자 '\r'
-        pass
-   else:
-        print(oneByte)
-        # buffer += oneByte.decode()
-
-print (buffer.strip())
+num = 0
+word = "비둘기"
+while 1:
+    num+=1
+    for _ in range(num):
+        print(word)
