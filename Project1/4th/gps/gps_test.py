1660b666e558332d832a720872c8b5121c02359b
sehooh5 <58541635+sehooh5@users.noreply.github.com>
2023-06-28 15:13:03 +0900

    update daily

--- Project1/4th/gps/gps_test.py
+++ Project1/4th/gps/gps_test.py
@@ -13,9 +13,8 @@ rows = c.fetchall()
 for row in rows:
     lat, lon = row
 #     print(f"GPS : {lat}, {lon}")
-    sys.stdout.write("\n")
-    sys.stdout.flush()
-    sys.stdout.write(f"\r    lat : {lat}  lon : {lon}")
+
+    sys.stdout.write(f"\r  lat : {lat}  lon : {lon}")
     sys.stdout.flush()
     time.sleep(0.5)
 
