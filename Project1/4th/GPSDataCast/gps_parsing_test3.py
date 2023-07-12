fb07a1e17db70fdbc4a2f60a4420c768cad00231
sehooh5 <58541635+sehooh5@users.noreply.github.com>
2023-07-12 11:29:41 +0900

    parsing gps data

--- Project1/4th/GPSDataCast/gps_parsing_test3.py
+++ Project1/4th/GPSDataCast/gps_parsing_test3.py
@@ -35,7 +35,7 @@ def get_data(db_file, table_name, column_name):
             data_hex = data[0].hex() # 숫자 문자열 형태
             data_hex_list = textwrap.wrap(data_hex, chunk_size)
             for data_hex_one in data_hex_list:
-            print(data_hex_one)
+                print(data_hex_one)
 
 
 #         print("hex : ",data_hex)
