12f1aedee081896e8dfc4fde0bc7da47802161b3
sehooh5 <58541635+sehooh5@users.noreply.github.com>
2023-07-13 15:47:44 +0900

    udpate parsing

--- Project1/4th/GPSParsing/gps_parsing.py
+++ Project1/4th/GPSParsing/gps_parsing.py
@@ -50,6 +50,7 @@ def get_data(db_path, table_name, column_name):
                     if len(data_list) == 10:
                         data_delimiter = data[0]
                         data_list = data_list[1:]
+                        print(data_delimiter, data_list)
                         if data_delimiter == '51':
                             print('Acceleration Output')
                             print(data_list)
