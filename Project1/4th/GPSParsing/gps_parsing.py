299f0fe755b7f98bb996bdf2c75bd65258df85f5
sehooh5 <58541635+sehooh5@users.noreply.github.com>
2023-07-18 12:18:39 +0900

    udpate parsing

--- Project1/4th/GPSParsing/gps_parsing.py
+++ Project1/4th/GPSParsing/gps_parsing.py
@@ -84,7 +84,9 @@ def get_data(db_path, table_name, column_name):
                             ay = (((data_int(data_list[2])<<8) | data_int(data_list[3]))/32768)*16
                             az = (((data_int(data_list[4])<<8) | data_int(data_list[5]))/32768)*16
                             temper = ((data_int(data_list[6])<<8) | data_int(data_list[7]))/100
-                            print(((data_int(data_list[0])<<8) | data_int(data_list[1])))
+
+                            test_x = ((data_int(data_list[0])<<8) | data_int(data_list[1]))
+                            print(float(test_x))
                             print(f'ax : {ax}, ay : {ay},az : {az},temperature : {temper}')
                             
                         elif data_delimiter == '52':
