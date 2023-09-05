defdc77fa985fd16475b4a10f5b48731533d958b
sehooh5 <58541635+sehooh5@users.noreply.github.com>
2023-09-05 17:52:17 +0900

    basic study about Perl

--- Basic/Perl/4_hash.pl
+++ Basic/Perl/4_hash.pl
@@ -6,6 +6,6 @@ my %hashValues = (
     "2" => "Two",
     "3" => "three", # trailing comma용
 );
-print $hashValues{"1"};
+print "프린트 $hashValues{"1"}";
 print $hashValues{"2"};
 print $hashValues{"3"};
