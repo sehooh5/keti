25ef2fa3e8de60e79b7a209ceba6837026167dee
sehooh5 <58541635+sehooh5@users.noreply.github.com>
2023-09-11 15:49:26 +0900

    update perl basic

---
+++ Basic/Perl/9_array.pl
@@ -0,0 +1,12 @@
+# 9. array 함수
+# array 자료형 뿐만 아니라 함수또한 지원
+
+my @array = (
+    "one", "twoo", "threee", "fourrrr"
+);
+
+print join(", ", @array), "\r\n";
+print "NORMAL : ", @array;
+print "REVERSE : ", reverse(@array), "\r\n";
+print join(", ", reverse(@array)), "\r\n";
+
