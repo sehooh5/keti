355c8869dc7e9a58933d1a17da7d980d31a6e7a0
sehooh5 <58541635+sehooh5@users.noreply.github.com>
2023-09-11 12:44:19 +0900

    update perl basic

--- Basic/Perl/8_loop.pl
+++ Basic/Perl/8_loop.pl
@@ -2,3 +2,30 @@
 # C나 PHP 루프문 선언과 비슷하다
 # array를 순회할때 for 문보다 foreach문이 편리할 떄가 많다.
 
+my @array = (
+    "one",
+    "2",
+    "3three"
+);
+
+my $i = 0;
+
+# while 문
+while($i < scalar @array) {
+    print $i, ": ", $array[$i];
+    $i++;
+}
+
+print "\n";
+
+# foreach 문
+foreach my $i ( 0 .. $array ) {
+    print $i, ": ", $array[$i];
+}
+
+print "\n";
+
+# for 문
+for(my $i = 0, $i < scalar @array, $i++) {
+    print $i, ": ", $array[$i];
+}
