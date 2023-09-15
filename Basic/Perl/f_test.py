60f76b14fc8ea2d9199b1cb2d089c57aa6c817be
sehooh5 <58541635+sehooh5@users.noreply.github.com>
2023-09-15 12:41:40 +0900

    update perl basic

--- Basic/Perl/f_test.py
+++ Basic/Perl/f_test.py
@@ -8,8 +8,31 @@ student_id = """2013105296
 2014131487"""
 major_id = {'10':'경영학과', '11':'사회학과', '12':'국문학과', '13':'물리학과'}
 
-s_ids = student_id.split()
+def count_id(student_id):
 
-for s_id in s_ids:
-    y = s_id[2]+s_id[3]
-    print(y)
+    s_ids = student_id.split()
+    cnt_y = {}
+    cnt_m = {}
+
+    for s_id in s_ids:
+        y = s_id[2]+s_id[3]
+        m_id = s_id[4]+s_id[5]
+        m = major_id[m_id]
+
+        if y in cnt_y:
+            cnt_y[y] += 1
+        else:
+            cnt_y[y] = 1
+
+        if m in cnt_m:
+            cnt_m[m] += 1
+        else:
+            cnt_m[m] = 1
+
+    print("기수별 학생 수:", dict(sorted(cnt_y.items())))
+    print("학과별 학생 수:", cnt_m)
+
+count_id(student_id)
+
+
+
