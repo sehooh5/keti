e40f69c7e71f730a56c42d9236bad300c424c90e
sehooh5 <58541635+sehooh5@users.noreply.github.com>
2023-10-11 14:56:51 +0900

    cpu_mem_test

--- Project6/1st/monitoring/cpu_mem_test.py
+++ Project6/1st/monitoring/cpu_mem_test.py
@@ -6,6 +6,6 @@ cpu_percent = psutil.cpu_percent(interval=1)  # 1초 동안의 CPU 사용률을 
 # 메모리 사용량 확인
 memory = psutil.virtual_memory()  # 가상 메모리 정보
 memory_percent = memory.percent  # 메모리 사용률을 백분율로 반환
-s
+
 print(f"CPU 사용률: {cpu_percent}%")
 print(f"메모리 사용률: {memory_percent}%")
