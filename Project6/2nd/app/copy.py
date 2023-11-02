b7be260ef84c99d249690e0c7e3af9780a49250c
sehooh5 <58541635+sehooh5@users.noreply.github.com>
2023-11-02 10:54:32 +0900

    update app.py about admin.conf copy

--- Project6/2nd/app/copy.py
+++ Project6/2nd/app/copy.py
@@ -2,12 +2,12 @@ import os
 import subprocess
 
 
-    os.system("echo yes | sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config")
-    command = "sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config"
-    result = subprocess.run(f"echo yes | {command}", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+os.system("echo yes | sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config")
+command = "sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config"
+result = subprocess.run(f"echo yes | {command}", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
 
-    if result.returncode == 0:
-        print("명령어 실행 성공.")
-    else:
-        print(f"오류 발생: {result.returncode}")
-        print(result.stderr)
+if result.returncode == 0:
+    print("명령어 실행 성공.")
+else:
+    print(f"오류 발생: {result.returncode}")
+    print(result.stderr)
