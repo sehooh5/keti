be22deb2c5143bdf3afac27acd20f4256c1843ac
sehooh5 <58541635+sehooh5@users.noreply.github.com>
2023-11-28 12:00:17 +0900

    extract_modules for admitting SW

--- Project1/4th/gps/blackbox_GUI/extract_modules.py
+++ Project1/4th/gps/blackbox_GUI/extract_modules.py
@@ -2,20 +2,30 @@
 
 import shutil
 import os
+import importlib.util
 
-# 프로그램에 사용된 모듈 목록
-used_modules = ['PyQt5.QtWidgets', 'PyQt5.QtCore', 'subprocess', 'os', 'sys', 'psutil']
-
-# 모듈을 저장할 디렉토리 생성
-output_directory = 'modules'
-os.makedirs(output_directory, exist_ok=True)
-
-# 모듈 파일을 복사하여 저장
-for module_name in used_modules:
-    module_path = shutil.which(module_name + '.py')
-    if module_path:
+def extract_and_save_module(module_name, output_directory):
+    # 모듈을 찾아서 저장
+    spec = importlib.util.find_spec(module_name)
+    if spec:
+        module_path = spec.origin
         shutil.copy(module_path, os.path.join(output_directory, module_name + '.py'))
     else:
         print(f"Module '{module_name}' not found.")
 
-print("Modules extracted successfully.")
+def main():
+    # 프로그램에 사용된 모듈 목록
+    used_modules = ['PyQt5.QtWidgets', 'PyQt5.QtCore', 'subprocess', 'os', 'sys', 'psutil']
+
+    # 모듈을 저장할 디렉토리 생성
+    output_directory = 'extracted_modules'
+    os.makedirs(output_directory, exist_ok=True)
+
+    # 각 모듈을 추출하고 저장
+    for module_name in used_modules:
+        extract_and_save_module(module_name, output_directory)
+
+    print("Modules extracted successfully.")
+
+if __name__ == "__main__":
+    main()
