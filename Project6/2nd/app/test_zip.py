d1ba3c86a5b8c5c16f2620f5194a06205969a44b
sehooh5 <58541635+sehooh5@users.noreply.github.com>
2023-11-29 15:37:30 +0900

    update upload_ai about zip file

---
+++ Project6/2nd/app/test_zip.py
@@ -0,0 +1,19 @@
+import zipfile
+import os
+
+file_path = "/home/edge-master-01"
+filename = "monitoring.zip"
+
+zip_file_path = "/home/edge-master-01/monitoring.zip"
+
+
+try:
+    # Zip 파일 열기
+    with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
+        # 압축 해제
+        zip_ref.extractall()
+    print(f"Zip file '{zip_file_path}' successfully extracted to '{extract_path}'.")
+except zipfile.BadZipFile as e:
+    print(f"Error: {zip_file_path} is not a valid zip file. {e}")
+except Exception as e:
+    print(f"Error extracting zip file: {e}")
