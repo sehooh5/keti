5f97427564af2ec71ff163444bb49a46dc6a36d2
sehooh5 <58541635+sehooh5@users.noreply.github.com>
2023-11-29 16:44:05 +0900

    update upload_ai about zip file

--- Project6/2nd/app/update.py
+++ Project6/2nd/app/update.py
@@ -7,4 +7,7 @@ print(result1)
 result2 = os.system("kubectl get pods | grep monitoring-edge-worker-02")
 print(result2)
 
+result3 = os.system("kubectl get pods | grep monitoring-edge-worker-03")
+print(result3)
+
 # result = subprocess.run(['whoami'], capture_output=True, text=True)
