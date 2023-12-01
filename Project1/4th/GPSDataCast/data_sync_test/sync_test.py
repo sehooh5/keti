d7fce352060fea48415de82efddf566064ad8eb4
sehooh5 <58541635+sehooh5@users.noreply.github.com>
2023-12-01 17:51:06 +0900

    update sync test

--- Project1/4th/GPSDataCast/data_sync_test/sync_test.py
+++ Project1/4th/GPSDataCast/data_sync_test/sync_test.py
@@ -12,13 +12,13 @@ instance = vlc.Instance("--no-xlib")  # X11 출력을 비활성화합니다.
 player = instance.media_player_new()
 
 # RTP 출력 설정
-rtp_output = ":rtp{{dst=192.168.0.14,port=5008,mux=ts}}"  # RTP 설정 예시, 포트 및 목적지 IP에 맞게 수정하세요
+rtp_output = ":rtp{dst=192.168.0.14,port=5008,mux=ts}"  # RTP 설정 예시, 포트 및 목적지 IP에 맞게 수정하세요
 
 # MP4 파일 경로
 media_path = "/home/edge-worker-01/blackbox_osan/blackbox_08.mp4"
 
 # 미디어 생성
-media = instance.media_new(media_path,"--loop --no-sout-all")
+media = instance.media_new(media_path)
 
 # 미디어 플레이어에 미디어 할당
 player.set_media(media)
@@ -27,12 +27,9 @@ player.set_media(media)
 player.play()  # 재생 시작
 
 # RTP 출력 설정 적용
-media.add_option(f"sout=#rtp{{dst=192.168.0.14,port=5008,mux=ts}}")
+media.add_option(rtp_output)
 
 # 루프 및 기타 설정
-# player.set_loop(True)  # 루프 설정
-
-# 이벤트 콜백 함수 등록
 events = player.event_manager()
 events.event_attach(vlc.EventType.MediaPlayerEndReached, lambda event: event_callback(event, player))
 
