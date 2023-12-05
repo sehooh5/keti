895b99e28f0d61fd89d10637d4b10a923b8d0d96
sehooh5 <58541635+sehooh5@users.noreply.github.com>
2023-12-05 10:58:31 +0900

    udpate sync test tested by python-vlc

--- Project1/4th/GPSDataCast/data_sync_test/gpsdatacast_parsed.py
+++ Project1/4th/GPSDataCast/data_sync_test/gpsdatacast_parsed.py
@@ -362,16 +362,11 @@ class App(QWidget):
             else:
 #                 command = f'cvlc /home/{username}/blackbox_osan/blackbox_0{num}.avi --sout "#rtp{{dst=192.168.0.14,port=500{num},mux=ts}}" --loop --no-sout-all' # 싱크 테스트
                 command = f'cvlc /home/{username}/blackbox_osan/blackbox_0{num}.avi --sout "#rtp{{dst=192.168.0.14,port=500{num},mux=ts}}" --no-sout-all' # 싱크 테스트
-            while True:
-                try:
-                    process_thead = subprocess.Popen(command, shell=True)
-                    setattr(self, f"process{num}_thread", process_thread)
-                    status_label = getattr(self, f"status{num}")
-                    status_label.setText(f'blackbox_0{num} RTP 전송중')
-                except subprocess.CalledProcessError as e:
-                    print(f'Error occured: {e}')
-                except KeboardInterrupt:
-                    break
+            process_thead = subprocess.Popen(command, shell=True)
+            setattr(self, f"process{num}_thread", process_thread)
+            status_label = getattr(self, f"status{num}")
+            status_label.setText(f'blackbox_0{num} RTP 전송중')
+
 
 #             if process_thread.poll() == None:
 #                 print("종료!!!!!")
--- Project1/4th/GPSDataCast/data_sync_test/sync_test.py
+++ Project1/4th/GPSDataCast/data_sync_test/sync_test.py
@@ -1,49 +1,22 @@
-import vlc
+import subprocess
+import shlex
 import time
 
-def event_callback(event, media_player):
-    if event.type == vlc.EventType.MediaPlayerEndReached:
-        print("재시작 합니다")
-        media_player.set_media_list(instance.media_list_new([media]))  # 미디어 리스트 재설정
-        media_player.play_item_at_index(0)  # 처음 항목 재생
-
-# VLC 초기화
-instance = vlc.Instance("--no-xlib")  # X11 출력을 비활성화합니다.
-media_player = instance.media_list_player_new()  # MediaListPlayer 생성
-
-# RTP 출력 설정
-num = 1  # 예시로 1을 사용하고 있습니다. 필요에 따라 수정하세요.
-rtp_output = f":rtp{{dst=192.168.0.14,port=5008,mux=ts}}"  # RTP 설정 예시, 포트 및 목적지 IP에 맞게 수정하세요
-
-# MP4 파일 경로
-num = 1  # 예시로 1을 사용하고 있습니다. 필요에 따라 수정하세요.
-mp4_path = f"/home/edge-worker-01/blackbox_osan/blackbox_08.mp4"
-
-
-# 미디어 생성
-media = instance.media_new(mp4_path)
-
-# 미디어 리스트 플레이어에 미디어 할당
-media_list = instance.media_list_new([media])
-media_player.set_media_list(media_list)
-
-# RTP로 스트리밍 설정
-media_player.play_item_at_index(0)  # 처음 항목 재생
-
-# RTP 출력 설정 적용
-media.get_mrl()  # 기존의 옵션을 삭제합니다.
-media.add_option(rtp_output)  # 새로운 RTP 출력 설정을 추가합니다.
-
-# 루프 및 기타 설정
-# media_player.set_fullscreen(True)  # 전체 화면 모드로 설정 (선택적)
-media_player.set_playback_mode(vlc.PlaybackMode.loop)  # 루프 설정
-
-# 이벤트 콜백 함수 등록
-events = media_player.event_manager()
-events.event_attach(vlc.EventType.MediaPlayerEndReached, lambda event: event_callback(event, media_player))
-
-try:
+def start_video_streaming(username, num):
     while True:
-        time.sleep(1)
-except KeyboardInterrupt:
-    media_player.stop()
+        command = f'cvlc /home/edge-worker-01/blackbox_osan/blackbox_08.avi --sout "#rtp{{dst=192.168.0.14,port=5008,mux=ts}}" --no-sout-all'
+        try:
+            subprocess.run(shlex.split(command), check=True)
+        except subprocess.CalledProcessError as e:
+            print(f"Error occurred: {e}")
+        except KeyboardInterrupt:
+            break
+
+if __name__ == "__main__":
+    username = "your_username"
+    stream_num = 1  # Adjust this value based on your requirements
+
+    try:
+        start_video_streaming(username, stream_num)
+    except KeyboardInterrupt:
+        print("Streaming stopped.")
