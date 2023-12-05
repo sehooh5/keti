4c03183aec14b7d7fbf9de5bf6c5dc0674c488ea
sehooh5 <58541635+sehooh5@users.noreply.github.com>
2023-12-05 11:22:35 +0900

    udpate sync test tested by python-vlc

--- Project1/4th/GPSDataCast/data_sync_test/sync_test.py
+++ Project1/4th/GPSDataCast/data_sync_test/sync_test.py
@@ -4,17 +4,18 @@ import time
 
 def start_video_streaming(username, num):
     while True:
-        command = f'cvlc /home/edge-worker-01/blackbox_osan/blackbox_08.mp4 --sout "#rtp{{dst=192.168.0.14,port=5008,mux=ts}}" --no-sout-all'
+        command = f'cvlc /home/{username}/blackbox_osan/blackbox_0{num}.avi --sout "#rtp{{dst=192.168.0.14,port=500{num},mux=ts}}" --no-sout-all --play-and-exit'
         try:
             subprocess.run(shlex.split(command), check=True)
+            print(f"Video stream {num} completed.")
         except subprocess.CalledProcessError as e:
             print(f"Error occurred: {e}")
         except KeyboardInterrupt:
             break
 
 if __name__ == "__main__":
-    username = "your_username"
-    stream_num = 1  # Adjust this value based on your requirements
+    username = "worker"
+    stream_num = 8  # Adjust this value based on your requirements
 
     try:
         start_video_streaming(username, stream_num)
