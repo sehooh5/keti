e451a28dfb7f1832bf45bf31a029a79252a779b6
sehooh5 <58541635+sehooh5@users.noreply.github.com>
2023-12-05 11:52:14 +0900

    udpate sync test tested by python-vlc

--- Project1/4th/GPSDataCast/data_sync_test/gpsdatacast_parsed.py
+++ Project1/4th/GPSDataCast/data_sync_test/gpsdatacast_parsed.py
@@ -357,8 +357,7 @@ class App(QWidget):
 
             if num == 8:
 #                 command = f'cvlc /home/{username}/blackbox_osan/blackbox_0{num}.mp4 --sout "#rtp{{dst=192.168.0.14,port=500{num},mux=ts}}" --loop --no-sout-all' # 싱크 테스트
-                command = f'cvlc /home/{username}/blackbox_osan/blackbox_0{num}.mp4 --sout "#rtp{{dst=192.168.0.14,port=500{num},mux=ts}}" --no-sout-all --play-and-exit' # 싱크 테스트
-            else:
+                command = f'cvlc /home/{username}/blackbox_osan/blackbox_0{num}.mp4 --sout "#rtp{{dst=192.168.0.14,port=500{num},mux=ts}}" --loop --no-sout-all --play-and-exit ' # 싱크 테스트            else:
 #                 command = f'cvlc /home/{username}/blackbox_osan/blackbox_0{num}.avi --sout "#rtp{{dst=192.168.0.14,port=500{num},mux=ts}}" --loop --no-sout-all' # 싱크 테스트
                 command = f'cvlc /home/{username}/blackbox_osan/blackbox_0{num}.avi --sout "#rtp{{dst=192.168.0.14,port=500{num},mux=ts}}" --no-sout-all --play-and-exit' # 싱크 테스트
             try:
