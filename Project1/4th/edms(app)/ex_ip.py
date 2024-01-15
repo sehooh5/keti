3c7b0797c49c6c3d8c597c3ef59c1b369b348092
sehooh5 <58541635+sehooh5@users.noreply.github.com>
2024-01-15 10:00:40 +0900

    update daily

--- Project1/4th/edms(app)/ex_ip.py
+++
@@ -1,7 +0,0 @@
-import socket
-
-print("Host Name ",socket.gethostname())
-
-print("IP Address(Internal) : ",socket.gethostbyname(socket.gethostname()))
-
-print("IP Address(External) : ",socket.gethostbyname(socket.getfqdn()))
