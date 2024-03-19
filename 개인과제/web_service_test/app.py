f174fd27e473bb17c78b56544a3f858af7aef113
sehooh5 <58541635+sehooh5@users.noreply.github.com>
2024-03-19 16:32:07 +0900

    update edge

--- 개인과제/web_service_test/app.py
+++ 개인과제/web_service_test/app.py
@@ -5,11 +5,12 @@ from datetime import datetime
 app = Flask(__name__)
 # app.config['JSON_AS_ASCII'] = False
 # CORS(app)
+port = "5000"
 
 @app.route('/')
 def index():
     print(datetime.now())
     return render_template('index.html')
 
-
- 
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', threaded=True, port=port)
