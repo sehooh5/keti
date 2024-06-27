e4e2537df1fc170ef10063d26db2c16e514d90f4
sehooh5 <58541635+sehooh5@users.noreply.github.com>
2024-06-27 16:19:34 +0900

    update 5g for device sw

--- Project1/4th/edms(app)/app.py
+++ Project1/4th/edms(app)/app.py
@@ -469,7 +469,7 @@ def add_newUploadSw():
             "%c")[:-4], f"{func}: docker image building...")
         print(f"명령어확인 ----- docker build -f {fname}/{fname} -t sehooh5/{fname}:latest .")
         os.system(
-            f"docker build -f {fname}/{fname} -t sehooh5/{fname}:latest .")
+            f"docker build -f . -t sehooh5/{fname}:latest .")
         print("Docker image building completed!!")
         # docker login status 확인
         try:
