b2d240a061b74f4269457039cd6150dd2e74c174
sehooh5 <58541635+sehooh5@users.noreply.github.com>
2024-07-01 17:08:02 +0900

    update 5g for device sw

--- Project1/3rd/edms(app)/k8s/monitoring_maker.py
+++ Project1/3rd/edms(app)/k8s/monitoring_maker.py
@@ -3,26 +3,26 @@ import os
 
 def making():
     os.system("kubectl create ns monitoring")
-    os.system("kubectl apply -f prometheus.yaml")
-    os.system("kubectl apply -f state_metrics.yaml")
-    os.system("kubectl apply -f grafana.yaml")
+    os.system("kubectl --kubeconfig=/home/keti2/.kube/config apply -f prometheus.yaml")
+    os.system("kubectl --kubeconfig=/home/keti2/.kube/config apply -f state_metrics.yaml")
+    os.system("kubectl --kubeconfig=/home/keti2/.kube/config apply -f grafana.yaml")
 
     return "Created!!"
 
 
 def namespace():
-    os.system("kubectl create ns monitoring")
+    os.system("kubectl --kubeconfig=/home/keti2/.kube/config create ns monitoring")
 
     return "Created!!"
 
 
 def prometheus(filename):
-    os.system(f"kubectl apply -f ./k8s/{filename}")
+    os.system(f"kubectl --kubeconfig=/home/keti2/.kube/config apply -f ./k8s/{filename}")
 
     return "Created!!"
 
 
 def grafana():
-    os.system("kubectl apply -f grafana.yaml")
+    os.system("kubectl --kubeconfig=/home/keti2/.kube/config apply -f grafana.yaml")
 
     return "Created!!"
