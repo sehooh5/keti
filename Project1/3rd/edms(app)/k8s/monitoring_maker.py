ac7bc1aad531b748b9468a43a906f9896e3cc4d3
sehooh5 <58541635+sehooh5@users.noreply.github.com>
2024-07-01 17:10:58 +0900

    update 5g for device sw

--- Project1/3rd/edms(app)/k8s/monitoring_maker.py
+++ Project1/3rd/edms(app)/k8s/monitoring_maker.py
@@ -1,28 +1,30 @@
 import os
 
 
+kubeconfig_path = f'/home/keti2/.kube/config'
+
 def making():
-    os.system("kubectl create ns monitoring")
-    os.system("kubectl --kubeconfig=/home/keti2/.kube/config apply -f prometheus.yaml")
-    os.system("kubectl --kubeconfig=/home/keti2/.kube/config apply -f state_metrics.yaml")
-    os.system("kubectl --kubeconfig=/home/keti2/.kube/config apply -f grafana.yaml")
+    os.system(f"kubectl create ns monitoring")
+    os.system(f"kubectl --kubeconfig={kubeconfig_path} apply -f prometheus.yaml")
+    os.system(f"kubectl --kubeconfig={kubeconfig_path} apply -f state_metrics.yaml")
+    os.system(f"kubectl --kubeconfig={kubeconfig_path} apply -f grafana.yaml")
 
     return "Created!!"
 
 
 def namespace():
-    os.system("kubectl --kubeconfig=/home/keti2/.kube/config create ns monitoring")
+    os.system(f"kubectl --kubeconfig={kubeconfig_path} create ns monitoring")
 
     return "Created!!"
 
 
 def prometheus(filename):
-    os.system(f"kubectl --kubeconfig=/home/keti2/.kube/config apply -f ./k8s/{filename}")
+    os.system(f"kubectl --kubeconfig={kubeconfig_path} apply -f ./k8s/{filename}")
 
     return "Created!!"
 
 
 def grafana():
-    os.system("kubectl --kubeconfig=/home/keti2/.kube/config apply -f grafana.yaml")
+    os.system(f"kubectl --kubeconfig={kubeconfig_path} apply -f grafana.yaml")
 
     return "Created!!"
